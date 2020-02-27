7932e14bb0715218f520b81bb60e9e46317114a6
Martin Lopez <mlo53@uclive.ac.nz>
2020-02-27 17:29:40 +1300

    Seperated CSS from profile vue file and experimented with vue actions
    the add email button does call the correct function, yet this function only prints a message to the console, next step is to get it to add the input email into a list of emails

--- client/src/pages/Profile/App.vue
+++ client/src/pages/Profile/App.vue
@@ -1,3 +1,5 @@
+
+
 <template>
   <div id="app">
     <div class="container">
@@ -5,13 +7,13 @@
     <hr> 
       <div class ="row">
         <div class="col-25">
-          <label for="fname"> First name</label>
+          <label for="fName"> First name:</label>
         </div>
         <div class="col-25">
-          <input type="text" id="fname" name="firstname" placeholder="John" required>
+          <input type="text" id="fName" name="firstName" placeholder="John" required>
         </div>
         <div class="col-25">
-          <label for="dob">Date of Birth</label>
+          <label for="dob">Date of Birth:</label>
         </div>
         <div class="col-25">
           <input type="date" id="dob" required>
@@ -19,45 +21,39 @@
       </div>
       <div class="row">
         <div class="col-25">
-          <label for="mname"> Middle</label>
+          <label for="lname">Last Name:</label>
         </div>
         <div class="col-25">
-          <input type="text" id="mname" name="middlename" placeholder="Middle">
+          <input type="text" id="lname" name="lname" placeholder="Doe" required>
         </div>
         <div class="col-25">
-          <label>Gender</label>
+          <label>Gender:</label>
         </div>
         <div class="col-25">
-          <div class="col-25">
-            <label for="male"><input type="radio" id="male" name="gender" value="male">F</label><br>
-          </div>
-          <div class="col-25">
-            <label for="female"><input type="radio" id="female" name="gender" value="female">M</label><br>
-          </div>
-          <div class="col-25">
-            <label for="other"><input type="radio" id="other" name="gender" value="other">N/A</label>
+            <div class="row">
+            <div class="col-25">
+              <label for="male"><input type="radio" id="male" name="gender" value="male">F</label><br>
+            </div>
+            <div class="col-25">
+              <label for="female"><input type="radio" id="female" name="gender" value="female">M</label><br>
+            </div>
+            <div class="col-25">
+              <label for="other"><input type="radio" id="other" name="gender" value="other">N/A</label>
+            </div>
           </div>
         </div>
       </div>
-      <div class="col-33">
-        <label for="lname"> Last name</label>
-        <input type="text" id="lname" name="lastname" placeholder="Doe" required>
-      </div>
-
       <div class="row">
-        <div class="col-50">
-          <label for="nName"> Nickname</label>
+        <div class="col-25">
+          <label for="nName"> Nickname:</label>
+        </div>
+        <div class="col-25">
           <input type="text" id="nName" name="lastname" placeholder="Johnny" required>
         </div>
-      </div>
-
-      <div class="row">
-        <div class="col-50">
-          <label for="email">Email address</label>
-          <input type="text" id="email" name="email" placeholder="john@example.com" required>
+        <div class="col-25">
+          <label for="fitnessLevel">Fitness Level:</label>
         </div>
-        <div class="col-50">
-          <label for="fitnessLevel">Fitness Level</label>
+        <div class="col-25">
           <select id="fitnessLevel">
             <option value="level0">Couch potato</option>
             <option value="level1">Coming back from injury</option>
@@ -67,15 +63,43 @@
           </select>
         </div>
       </div>
-
       <div class="row">
         <div class="col-50">
-          <label for="password"><b>Password</b></label>
-          <input type="password" id="password" name="password" placeholder="8-10 Characters" required>
+          <label for="bio">Bio:</label>
         </div>
         <div class="col-50">
-          <label for="psw-repeat"><b>Repeat Password</b></label>
-          <input type="password" id="psw-repeat" placeholder="Repeat Password" name="psw-repeat" required>
+          <input type="body" id="bio" name="bio" placeholder="Something interesting about you!">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-25">
+          <label for="emailInput">Email address:</label>
+        </div>
+        <div class="col-50">
+          <input type="text" id="emailInput" name="email" placeholder="john@example.com" required>
+        </div>
+        <div id="emailAdd" class="col-25">
+          <button v-on:click="addEmail" >Add Email</button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-50">
+          <table>
+            <th>
+              Available Passport countries
+            </th>
+            <th>
+              Your Passport countries
+            </th>
+            <tr>
+              <td>New Zealand</td>
+              <td>Australia</td>
+            </tr>
+            <tr>
+              <td>Chile</td>
+              <td>China</td>
+            </tr>
+          </table>
         </div>
       </div>
     </div>
@@ -86,89 +110,33 @@
 </template>
 
 <script>
-//   import Students from './components/Students'
+
   // app Vue instance
-  
+
+  const User = {
+    el: '#emailAdd',
+    data: {
+      emails: [],
+    },
+    methods: {
+      // button calls function properly but unsure how to add email input into list of emails
+      addEmail: function () {
+        var newEmail = document.getElementById("emailInput").value;
+        console.log(newEmail);
+        // this.emails.push(newEmail);
+        // console.log(this.emails);
+      }
+    }
+  };
+
+  export default User
+
 </script>
 
 <style scoped>
+  @import 'Profile.css';
+
   [v-cloak] { display: none; }
 
-.row {
-  display: -ms-flexbox; /* IE10 */
-  display: flex;
-}
 
-.col-10{
-  -ms-flex: 10%; /* IE10 */
-  flex: 10%;
-}
-
-.col-25 {
-  -ms-flex: 25%;
-  flex: 25%;
-}
-
-.col-40 {
-  -ms-flex: 40%; /* IE10 */
-  flex: 40%;
-}
-
-.col-50 {
-  -ms-flex: 50%; /* IE10 */
-  flex: 50%;
-}
-
-.col-33 {
-  -ms-flex: 33%; /* IE10 */
-  flex: 33%;
-}
-
-.col-100{
-  -ms-flex: 100%;
-  flex: 100%;
-}
-
-.col-25,
-.col-33,
-.col-50,
-.col-100{
-  padding: 0 16px;
-}
-
-input[type=text] {
-  width: 100%;
-  margin-bottom: 20px;
-  padding: 12px;
-  border: 1px solid #ccc;
-  border-radius: 3px;
-}
-input[type=password] {
-  width: 100%;
-  margin-bottom: 20px;
-  padding: 12px 12px 12px 12px;
-  border: 1px solid #ccc;
-  border-radius: 3px;
-}
-
-input[type=radio] {
-  padding: 0;
-  margin: 0;
-  border: 0;
-  height: 1px;
-  width: 1px;
-
-}
-
-label {
-  margin-bottom: 10px;
-  display: block;
-}
-
-.container {
-  background-color: #f2f2f2;
-  padding: 5px 20px 15px 20px;
-  border: 1px solid lightgrey;
-  border-radius: 3px;
-}
 </style>
