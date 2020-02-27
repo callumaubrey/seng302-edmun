7bcb41df77594c086beedb0ce59be96f4baea8e1
Cheng Yi Kok <cyk19@uclive.ac.nz>
2020-02-27 19:01:53 +1300

    Did initial work on the validation of form in the registration page
    - Created validationFields function that would iterate every input field and check its validation, currently it does not cover every cases
    
    #pair[cyk19, ydu34] #story[31] #implement

--- client/src/pages/Registration/App.vue
+++ client/src/pages/Registration/App.vue
@@ -1,94 +1,97 @@
 <template>
   <div id="app">
     <div class="container">
-    <h1> Create a new account </h1>
-    <hr>
-      <div class ="row">
-        <div class="col-33">
-          <label class="field" for="fname"> First name</label>
-          <input type="text" id="fname" name="firstname" placeholder="John" required>
+      <h1> Create a new account </h1>
+      <hr>
+      <form method="post">
+        <div class="row">
+          <div class="col-33">
+            <label class="field" for="fname"> First name</label>
+            <input id="fname" name="firstname" placeholder="John" required type="text">
+          </div>
+          <div class="col-33">
+            <label class="field" for="mname"> Middle name</label>
+            <input id="mname" name="middlename" placeholder="Middle" type="text">
+          </div>
+          <div class="col-33">
+            <label class="field" for="lname"> Last name</label>
+            <input id="lname" name="lastname" placeholder="Doe" required type="text">
+          </div>
         </div>
-        <div class="col-33">
-          <label class="field" for="mname"> Middle name</label>
-          <input type="text" id="mname" name="middlename" placeholder="Middle">
-        </div>
-        <div class="col-33">
-          <label class="field" for="lname"> Last name</label>
-          <input type="text" id="lname" name="lastname" placeholder="Doe" required>
-        </div>
-      </div>
 
-      <div class="row">
-        <div class="col-100">
-          <label class="field" for="nname"> Nickname</label>
+        <div class="row">
+          <div class="col-100">
+            <label class="field" for="nname"> Nickname</label>
+          </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-100">
-          <input type="text" id="nname" name="nickname" placeholder="Johnny">
+        <div class="row">
+          <div class="col-100">
+            <input id="nname" name="nickname" placeholder="Johnny" type="text">
+          </div>
         </div>
-      </div>
 
 
-      <div class="row">
-        <div class="col-100">
-          <label class="field" for="email">Email address</label>
+        <div class="row">
+          <div class="col-100">
+            <label class="field" for="email">Email address</label>
+          </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-100">
-          <input type="text" id="email" name="email" placeholder="john@example.com" required>
+        <div class="row">
+          <div class="col-100">
+            <input id="email" name="email" placeholder="john@example.com" required type="email">
+          </div>
         </div>
-      </div>
 
-      <div class="row">
-        <div class="col-50">
-          <label class="field" for="psw">Password</label>
-          <input type="password" id="psw" name="password" placeholder="Password" required>
+        <div class="row">
+          <div class="col-50">
+            <label class="field" for="psw">Password</label>
+            <input id="psw" name="password" placeholder="Password" required type="password">
+          </div>
+          <div class="col-50">
+            <label class="field" for="psw-repeat">Repeat Password</label>
+            <input id="psw-repeat" name="psw-repeat" placeholder="Repeat Password" required type="password">
+          </div>
         </div>
-        <div class="col-50">
-          <label class="field" for="psw-repeat">Repeat Password</label>
-          <input type="password" id="psw-repeat" placeholder="Repeat Password" name="psw-repeat" required>
-        </div>
-      </div>
-      <div class ="col-100">
-        <label class="field" for="dob">Date of birth</label>
-      </div>
-      <div class ="row">
         <div class="col-100">
-          <date-dropdown
-                  :max="currentYear"
-                  id="dob"
-                  min="1900"
-                  months-names="Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec"
-                  v-model="selectedDate">
-          </date-dropdown>
+          <label class="field" for="dob">Date of birth</label>
         </div>
-      </div>
+        <div class="row">
+          <div class="col-100">
+            <date-dropdown
+                    :max="currentYear"
+                    id="dob"
+                    min="1900"
+                    months-names="Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec"
+                    v-model="selectedDate">
+            </date-dropdown>
+          </div>
+        </div>
 
-      <div class="row">
-        <div class="col-100">
-          <label class="field">Gender</label>
+        <div class="row">
+          <div class="col-100">
+            <label class="field">Gender</label>
+          </div>
         </div>
-      </div>
 
-      <div class="row">
-        <div class="col-33">
-          <label for="male"><input type="radio" id="male" name="gender" value="male">Female</label><br>
+        <div class="row">
+          <div class="col-33">
+            <label for="male"><input id="male" name="gender" type="radio" value="male">Female</label><br>
+          </div>
+          <div class="col-33">
+            <label for="female"><input id="female" name="gender" type="radio" value="female">Male</label><br>
+          </div>
+          <div class="col-33">
+            <label for="other"><input id="other" name="gender" type="radio" value="other">Non-binary</label>
+          </div>
         </div>
-        <div class="col-33">
-          <label for="female"><input type="radio" id="female" name="gender" value="female">Male</label><br>
-        </div>
-        <div class="col-33">
-          <label for="other"><input id="other" name="gender" type="radio" value="other">Non-binary</label>
-        </div>
-      </div>
 
-      <div class="row">
-        <div class="col-100">
-          <button class="sign-up-btn" type="button">Sign Up</button>
+        <div class="row">
+          <div class="col-100">
+            <button class="sign-up-btn" type="button">Sign Up</button>
+          </div>
         </div>
-      </div>
+        <input :onClick="this.validateFields()" type="submit" value="Submit">
+      </form>
 
     </div>
   </div>
@@ -101,13 +104,37 @@
     components: {
       DateDropdown
     },
-    data () {
+    data() {
       return {
         selectedDate: '',
         currentYear: (new Date).getFullYear().toString(),
       }
+    },
+    methods: {
+      validateFields() {
+        var elements = document.getElementsByTagName("INPUT");
+        for (var i = 0; i < elements.length; i++) {
+          elements[i].onclick = function (e) {
+            e.target.style.borderColor = "white";
+          };
+          elements[i].oninvalid = function (e) {
+            e.target.setCustomValidity("");
+            if (!e.target.validity.valid) {
+              e.target.setCustomValidity("This field cannot be left blank");
+              e.target.style.borderColor = "red";
+            }
+          };
+          elements[i].oninput = function (e) {
+            e.target.setCustomValidity("");
+
+          };
+        }
+
+      }
     }
   }
+
+
 
 </script>
 
