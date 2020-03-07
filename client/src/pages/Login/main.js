599c01920a480d88239baebd2b3dc08defd5e214
Yu Duan <ydu34@uclive.ac.nz>
2020-03-07 17:19:19 +1300

    Implemented login page, front end validation and post request
    - These were implemented with BootstrapVue
    - Post request implemented with Axios
    - However post request is blocked by CORS policy
    
    #implement #story[182]

--- client/src/pages/Login/main.js
+++ client/src/pages/Login/main.js
@@ -1,25 +1,35 @@
 import Vue from 'vue'
 import App from './App'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueLogger from 'vuejs-logger';
+import { BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false;
 
-import VueLogger from 'vuejs-logger';
-
 const options = {
   isEnabled: true,
-  logLevel : 'debug',
-  stringifyArguments : false,
-  showLogLevel : true,
-  showMethodName : false,
+  logLevel: 'debug',
+  stringifyArguments: false,
+  showLogLevel: true,
+  showMethodName: false,
   separator: '|',
   showConsoleColors: true
 };
 
 Vue.use(VueLogger, options);
+Vue.use(VueAxios, axios);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App }
-});
+  components: {App},
+  render: h => h(App),
+}).$mount('#app');
+
