4beb08790b8931fbea4eb800ec5d4cee5c841de2
Callum Aubrey <csraubrey@gmail.com>
2020-03-11 11:30:28 +1300

    Added new Profile page skeleton #implement #pair[csa80, ewm27] #story[182]

---
+++ client/src/pages/Profile/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+        <NavBar isLoggedIn=true></NavBar>
+
+        <div class="container">
+            <b-container fluid>
+                <b-row>
+                    <b-col>
+                        <h4>Callum Aubrey</h4>
+                        <hr>
+                    </b-col>
+                </b-row>
+            </b-container>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+    // import api from '../Api';
+    import NavBar from "@/components/NavBar.vue"
+
+    export default {
+        components: {
+            NavBar
+        }
+    }
+</script>
