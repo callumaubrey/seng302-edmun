e0491da4b6377392bb1300b73793690e50c16f3f
Harry Collard <htc36@uclive.ac.nz>
2020-03-17 11:58:38 +1300

    Added ability to view activites of a user on the profile page. Also changed the layout of the profile page as it was too basic and wasnt spaced out very well. Implented tabs to combat this #story[#188] #implement

--- client/src/pages/Profile/App.vue
+++ client/src/pages/Profile/App.vue
@@ -3,96 +3,116 @@
         <NavBar v-bind:isLoggedIn="isLoggedIn"></NavBar>
 
         <div class="container">
-            <b-container fluid>
+            <div>
                 <b-row>
-                    <b-col>
-                        <h3>{{userData.firstname}} {{userData.lastname}}</h3>
-                        <hr>
-                    </b-col>
+                <b-img center v-bind="mainProps" rounded= "circle" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.f4t8FtSbfNUs94KKgMhhBAHaHr%26pid%3DApi%26h%3D160&f=1" alt="Center image"></b-img>
                 </b-row>
-            </b-container>
+                <b-row><h3></h3></b-row>
+                <b-row align-h="center">
+                    <h3>{{userData.firstname}} {{userData.lastname}} </h3>
+                </b-row>
+                <b-tabs content-class="mt-3" align="center">
+                    <b-tab title="About" active>
+                        <b-card style="margin: 1em" title="About:">
+                            <b-row>
+                                <b-col><b>Nickname:</b></b-col>
+                                <b-col v-if="userData.nickName != null"><p >{{userData.nickName}}</p></b-col>
+                                <b-col v-else><p>No nickname</p></b-col>
+                            </b-row>
+                            <b-row>
+                                <b-col><b>Date of Birth:</b></b-col>
+                                <b-col><p>{{userData.dob}}</p></b-col>
+                            </b-row>
+                            <b-row>
+                                <b-col><b>Gender:</b></b-col>
+                                <b-col><p>{{userData.gender}}</p></b-col>
+                            </b-row>
+                        </b-card>
+                    </b-tab>
 
-            <b-row>
-                <b-col>
-                    <b-card style="margin: 1em" title="About:">
-                        <b-row>
-                            <b-col><b>Nickname:</b></b-col>
-                            <b-col v-if="userData.nickName != null"><p >{{userData.nickName}}</p></b-col>
-                            <b-col v-else><p>No nickname</p></b-col>
-                        </b-row>
-                        <b-row>
-                            <b-col><b>Date of Birth:</b></b-col>
-                            <b-col><p>{{userData.dob}}</p></b-col>
-                        </b-row>
-                        <b-row>
-                            <b-col><b>Gender:</b></b-col>
-                            <b-col><p>{{userData.gender}}</p></b-col>
-                        </b-row>
-                    </b-card>
-
-                    <b-card style="margin: 1em" title="Email(s):">
-                        <b-row>
-                            <b-col><b>Primary Email:</b></b-col>
-                            <b-col><p>{{userData.email}}</p></b-col>
-                        </b-row>
-                        <b-row>
-                            <b-col><b>Additional Emails:</b></b-col>
-                            <b-col v-if="additionalEmails == []">
-                                <p>No additional emails</p>
-                            </b-col>
-                            <b-col v-else>
-                                <p>
+                    <b-tab title="Email">
+                        <b-card style="margin: 1em" title="Email(s):">
+                            <b-row>
+                                <b-col><b>Primary Email:</b></b-col>
+                                <b-col><p>{{userData.email}}</p></b-col>
+                            </b-row>
+                            <b-row>
+                                <b-col><b>Additional Emails:</b></b-col>
+                                <b-col v-if="additionalEmails == []">
+                                    <p>No additional emails</p>
+                                </b-col>
+                                <b-col v-else>
+                                    <p>
                                     <ul>
                                         <li v-for="item in additionalEmails" :key="item.email">
                                             {{ item.email }}
                                         </li>
                                     </ul>
-                                </p>
-                            </b-col>
-                        </b-row>
-                        <p>(Max 5 emails)</p>
-                    </b-card>
-                </b-col>
-
-                <b-col>
-                    <b-card style="margin: 1em" title="Passport Info:">
-                        <b-row>
-                            <b-col><b>Your Passport Countries:</b></b-col>
-                            <b-col>
-                                <p>
+                                    </p>
+                                </b-col>
+                            </b-row>
+                            <p>(Max 5 emails)</p>
+                        </b-card>
+                    </b-tab>
+                    <b-tab title="Passport Info" >
+                        <b-card style="margin: 1em" title="Passport Info:">
+                            <b-row>
+                                <b-col><b>Your Passport Countries:</b></b-col>
+                                <b-col>
+                                    <p>
                                     <ul>
                                         <li v-for="item in passports" :key="item.countryName">
                                             {{ item.countryName }}
                                         </li>
                                     </ul>
-                                </p>
-                            </b-col>
-                        </b-row>
-                    </b-card>
+                                    </p>
+                                </b-col>
+                            </b-row>
+                        </b-card>
 
-                    <b-card style="margin: 1em" title="Fitness Info:">
-                        <b-row>
-                            <b-col><b>Fitness Level:</b></b-col>
-                            <b-col v-if="userData.fitness == 1"><p>Couch potato</p></b-col>
-                            <b-col v-if="userData.fitness == 2"><p>Some activity</p></b-col>
-                            <b-col v-if="userData.fitness == 3"><p>Average fitness level</p></b-col>
-                            <b-col v-if="userData.fitness == 4"><p>Above average fitness</p></b-col>
-                            <b-col v-if="userData.fitness == 5"><p>Serious athlete</p></b-col>
-                            <b-col v-else><p>Edit your profile to add a fitness level!</p></b-col>
-                        </b-row>
-                    </b-card>
+                    </b-tab>
+                    <b-tab title="Fitness Info" >
+                        <b-card style="margin: 1em" title="Fitness Info:">
+                            <b-row>
+                                <b-col><b>Fitness Level:</b></b-col>
+                                <b-col v-if="userData.fitness == 1"><p>Couch potato</p></b-col>
+                                <b-col v-if="userData.fitness == 2"><p>Some activity</p></b-col>
+                                <b-col v-if="userData.fitness == 3"><p>Average fitness level</p></b-col>
+                                <b-col v-if="userData.fitness == 4"><p>Above average fitness</p></b-col>
+                                <b-col v-if="userData.fitness == 5"><p>Serious athlete</p></b-col>
+                                <b-col v-else><p>Edit your profile to add a fitness level!</p></b-col>
+                            </b-row>
+                        </b-card>
+                    </b-tab>
 
-                    <b-card style="margin: 1em;" title="Location Info:">
-                        <b-input id="locationInput" class="form-control" type="text" @keyup.native="getLocationData"></b-input>
+                    <b-tab title="Location Info" >
+                        <b-card style="margin: 1em;" title="Location Info:">
+                            <b-input id="locationInput" class="form-control" type="text" @keyup.native="getLocationData"></b-input>
                             <div v-for="i in locations" :key="i.place_id">
                                 <b-input v-on:click="setLocationInput(i)" type="button" :value=i.display_name></b-input>
                             </div>
-                    </b-card>
-                </b-col>
+                        </b-card>
 
-            </b-row>
+                    </b-tab>
+                    <b-tab title="Activity Info" >
+                        <b-card style="margin: 1em" title="Passport Info:">
+                            <b-row>
+                                <b-col><b>Your Activities</b></b-col>
+                                <b-col>
+                                    <p>
+                                    <ul>
+                                        <li v-for="item in activites" :key="item.activity">
+                                            {{ item.activity }}
+                                        </li>
+                                    </ul>
+                                    </p>
+                                </b-col>
+                            </b-row>
+                        </b-card>
 
-
+                    </b-tab>
+                </b-tabs>
+            </div>
 
         </div>
     </div>
@@ -113,6 +133,7 @@
             return {
                 userData: '',
                 passports: [],
+                activites: [],
                 additionalEmails: [],
                 isLoggedIn: false,
                 locations: []
@@ -127,6 +148,7 @@
                         console.log(response.data);
                         currentObj.userData = response.data;
                         currentObj.passports = response.data.passports;
+                        currentObj.activites = response.data.activityTypes;
                         currentObj.additionalEmails = response.data.additionalemail;
                         currentObj.isLoggedIn = true;
                     })
