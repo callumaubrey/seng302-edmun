09cd1ca53b5bfebfe233fa79a90aa7a5067522fb
Yu Duan <ydu34@uclive.ac.nz>
2020-03-20 19:05:49 +1300

    Wrote unit tests for the PUT /profiles/{profileId} endpoint - Currently unit tests does not use a mock database - Tests take a long time to run - Fixed bugs that come up while testing #fix #test #story[183]

--- server/src/main/java/com/springvuegradle/team6/requests/EditProfileRequest.java
+++ server/src/main/java/com/springvuegradle/team6/requests/EditProfileRequest.java
@@ -16,25 +16,23 @@ public class EditProfileRequest {
     /**
      * New first name for profile unchanged if empty
      */
-    @NotNull
-    @NotEmpty
-    @Pattern(regexp = "^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$")
+    @NotNull(message = "firstname must be included")
+    @NotEmpty(message = "firstname cannot be empty")
+    @Pattern(regexp = "^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$", message = "firstname is invalid")
     public String firstname;
 
     /**
      * New middle name for profile unchanged if empty
      */
-    @NotNull
-    @NotEmpty
-    @Pattern(regexp = "^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$")
+    @Pattern(regexp = "^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$", message = "middlename is invalid")
     public String middlename;
 
     /**
      * New last name for profile unchanged if empty
      */
-    @NotNull
-    @NotEmpty
-    @Pattern(regexp = "^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$")
+    @NotNull(message = "lastname cannot be empty")
+    @NotEmpty(message = "lastname cannot be empty")
+    @Pattern(regexp = "^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$", message = "lastname is invalid")
     public String lastname;
 
     /**
@@ -50,23 +48,25 @@ public class EditProfileRequest {
     /**
      * New date of birth for profile unchanged if empty
      */
-    @NotNull
-    @NotEmpty
+    @NotNull(message = "date_of_birth must be included")
+    @NotEmpty(message = "date_of_birth cannot be empty")
+    @Pattern(regexp = "([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))", message = "date of birth is invalid")
     @JsonProperty("date_of_birth")
     public String dob;
 
     /**
      * New gender for profile unchanged if empty
      */
-    @NotNull
-    @NotEmpty
-    @Pattern(regexp = "(male)|(female)|(nonbinary)")
+    @NotNull(message = "gender must be included")
+    @NotEmpty(message = "gender cannot be empty")
+    @Pattern(regexp = "(male)|(female)|(nonbinary)", message = "gender is invalid")
     public String gender;
 
     /**
      * New fiteness level for profile unchanged if empty
      */
-    @Min(value = 0) @Max(value = 4)
+    @Min(value = 0, message = "fitness cannot be less than 0")
+    @Max(value = 4, message = "fitness cannot be larger than 4")
     public Integer fitness = 0;
 
     /**
@@ -79,16 +79,16 @@ public class EditProfileRequest {
      */
     public List<@Length(min = 3, max = 3) String> passports;
 
-    @NotNull
-    @NotEmpty
+    @NotNull(message = "primary_email must be included")
+    @NotEmpty(message = "primary_email cannot be empty")
     @JsonProperty("primary_email")
-    @javax.validation.constraints.Email(message = "Email should be valid")
+    @javax.validation.constraints.Email(message = "Email is invalid")
     public String primaryemail;
 
     /**
      * List of additional emails
      */
-    @EmailCollection @Size(min=0, max=5)
+    @EmailCollection @Size(max=5)
     public List<String> additionalemail;
 
     /**
