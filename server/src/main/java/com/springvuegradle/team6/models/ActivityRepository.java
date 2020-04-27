d38cb6f479a7c8900c6231fa6c6d2a1466f41c8a
Harry Collard <htc36@uclive.ac.nz>
2020-04-28 11:11:38 +1200

    Created base activity repository class with additional find activity by id, had to comment out until the activity class has been created. Also got familiar with the backend after the break we've had

---
+++ server/src/main/java/com/springvuegradle/team6/models/ActivityRepository.java
@@ -0,0 +1,16 @@
+//package com.springvuegradle.team6.models;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+//
+//@RepositoryRestResource
+//public interface ActivityRepository extends JpaRepository<Activity, String> {
+//    /**
+//     * Find activity by activity id
+//     *
+//     * @param id activity id
+//     * @return object of activity class
+//     */
+//
+//    Activity findById(int id);
+//}
