7a194e201021bfc7a18f844263381c82cd258f6b
Yu Duan <ydu34@uclive.ac.nz>
2020-06-30 17:18:39 +1200

    Implemented TagRepository method to query database to find tags that match the search for auto complete
    - This method will order the search by the number of activities associated to it.
    - Tests are written to test the query method.
    
    #test #implement #story[287]

--- server/src/main/java/com/springvuegradle/team6/models/TagRepository.java
+++ server/src/main/java/com/springvuegradle/team6/models/TagRepository.java
@@ -1,6 +1,9 @@
 package com.springvuegradle.team6.models;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.List;
 
@@ -10,4 +13,8 @@ public interface TagRepository extends JpaRepository<Tag, Integer> {
   List<Tag> findByNameContaining(String name);
 
   List<Activity> findActivitiesByName(String name);
+
+  @Query(
+      "SELECT t.name from Tag t left join t.activities a where t.name like :search% group by (t.name) ORDER BY COUNT(t.name) DESC")
+  List<String> findTagsMatchingSearch(@Param("search") String search);
 }
---
+++ server/src/test/java/com/springvuegradle/team6/models/TagRepositoryTest.java
@@ -0,0 +1,110 @@
+package com.springvuegradle.team6.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@SpringBootTest
+@TestPropertySource(properties = {"ADMIN_EMAIL=test@test.com", "ADMIN_PASSWORD=test"})
+public class TagRepositoryTest {
+
+  @Autowired private TagRepository tagRepository;
+  @Autowired private ActivityRepository activityRepository;
+  @Autowired private ProfileRepository profileRepository;
+
+  private static boolean dataLoaded = false;
+
+  @BeforeEach
+  void setup() {
+    if (!dataLoaded) {
+      Set<Email> emails = new HashSet<>();
+      Email email = new Email("johnydoe99@gmail.com");
+      emails.add(email);
+      Profile profile = new Profile();
+      profile.setFirstname("John");
+      profile.setLastname("Doe");
+      profile.setEmails(emails);
+      profile.setDob("2010-01-01");
+      profile.setPassword("Password1");
+      profile.setGender("male");
+      profile = profileRepository.save(profile);
+
+      Tag cool = new Tag();
+      cool.setName("#cool");
+      cool = tagRepository.save(cool);
+
+      Tag cold = new Tag();
+      cold.setName("#cold");
+      cold = tagRepository.save(cold);
+
+      Tag colour = new Tag();
+      colour.setName("#colour");
+      colour = tagRepository.save(colour);
+
+      Tag awesome = new Tag();
+      awesome.setName("#awesome");
+      awesome = tagRepository.save(awesome);
+
+      Activity activity = new Activity();
+      activity.setProfile(profile);
+      activity.setActivityName("Run at Hagley Park");
+      activity.setContinuous(true);
+      Set<Tag> tags = new HashSet<>();
+      tags.add(cold);
+      tags.add(cool);
+      activity.setTags(tags);
+      activity = activityRepository.save(activity);
+
+      Activity activity1 = new Activity();
+      activity1.setProfile(profile);
+      activity1.setActivityName("Avonhead Park Walk");
+      activity1.setContinuous(true);
+      Set<Tag> tags1 = new HashSet<>();
+      tags1.add(cold);
+      tags1.add(colour);
+      activity1.setTags(tags1);
+      activity1 = activityRepository.save(activity1);
+
+      Activity activity2 = new Activity();
+      activity2.setProfile(profile);
+      activity2.setActivityName("Burnside Park Rugby");
+      activity2.setContinuous(true);
+      Set<Tag> tags2 = new HashSet<>();
+      tags2.add(cold);
+      tags2.add(awesome);
+      tags2.add(cool);
+      activity2.setTags(tags2);
+      activity2 = activityRepository.save(activity2);
+
+      dataLoaded = true;
+    }
+  }
+
+  @Test
+  void testFindTagsMatchingSearchSingleResult() {
+    List<String> result = tagRepository.findTagsMatchingSearch("#awes");
+    List<String> expectedResult = new ArrayList<>();
+    expectedResult.add("#awesome");
+    org.junit.jupiter.api.Assertions.assertEquals(expectedResult, result);
+  }
+
+  @Test
+  void testFindTagsMatchingSearchMultipleResult() {
+    List<String> result = tagRepository.findTagsMatchingSearch("#co");
+    List<String> expectedResult = new ArrayList<>();
+    expectedResult.add("#cold");
+    expectedResult.add("#cool");
+    expectedResult.add("#colour");
+    org.junit.jupiter.api.Assertions.assertEquals(expectedResult, result);
+  }
+}
