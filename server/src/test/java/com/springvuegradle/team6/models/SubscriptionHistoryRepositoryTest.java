773e355d5ad62b88c6a7893cc696552e6769f67e
Louis Davies <lda62@uclive.ac.nz>
2020-07-03 15:25:28 +1200

    Fixed bug with manual ids in test and added one more test #fix #test

--- server/src/test/java/com/springvuegradle/team6/models/SubscriptionHistoryRepositoryTest.java
+++ server/src/test/java/com/springvuegradle/team6/models/SubscriptionHistoryRepositoryTest.java
@@ -21,73 +21,75 @@ public class SubscriptionHistoryRepositoryTest {
     @Autowired private ActivityRepository activityRepository;
     @Autowired private ProfileRepository profileRepository;
 
-    private static boolean dataLoaded = false;
-
     @BeforeEach
     void setup() {
-        if (!dataLoaded) {
-            Set<Email> emails = new HashSet<>();
-            Email email = new Email("johnydoe99@gmail.com");
-            emails.add(email);
-            Profile profile = new Profile();
-            profile.setId(1);
-            profile.setFirstname("John");
-            profile.setLastname("Doe");
-            profile.setEmails(emails);
-            profile.setDob("2010-01-01");
-            profile.setPassword("Password1");
-            profile.setGender("male");
-            profile = profileRepository.save(profile);
-
-            Set<Email> emails1 = new HashSet<>();
-            Email email1 = new Email("thesecondemail@gmail.com");
-            emails1.add(email1);
-            Profile profile1 = new Profile();
-            profile.setId(2);
-            profile1.setFirstname("Gon");
-            profile1.setLastname("Freecss");
-            profile1.setEmails(emails1);
-            profile1.setDob("2010-01-01");
-            profile1.setPassword("Password1");
-            profile1.setGender("male");
-            profile1 = profileRepository.save(profile1);
-
-            activityRepository.deleteAll();
-
-            Activity activity = new Activity();
-            activity.setProfile(profile);
-            activity.setActivityName("Run at Hagley Park");
-            activity.setContinuous(true);
-            activity = activityRepository.save(activity);
-
-            Activity activity1 = new Activity();
-            activity1.setProfile(profile);
-            activity1.setActivityName("Avonhead Park Walk");
-            activity1.setContinuous(true);
-            activity1 = activityRepository.save(activity1);
-
-            Activity activity2 = new Activity();
-            activity2.setProfile(profile);
-            activity2.setActivityName("Burnside Park Rugby");
-            activity2.setContinuous(true);
-            activity2 = activityRepository.save(activity2);
-
-            SubscriptionHistory subscriptionHistory = new SubscriptionHistory();
-            subscriptionHistory.setActivity(activity);
-            subscriptionHistory.setProfile(profile);
-            subscriptionHistory.setSubscribe(true);
-            subscriptionHistory.setTimeDate(new Date());
-            subscriptionHistory = subscriptionHistoryRepository.save(subscriptionHistory);
-
-            dataLoaded = true;
-        }
+        subscriptionHistoryRepository.deleteAll();
+        activityRepository.deleteAll();
+        profileRepository.deleteAll();
     }
 
     @Test
     void testSingleSubscriptionFindByProfile() {
-        Set<SubscriptionHistory> subscriptionHistories = subscriptionHistoryRepository.findByProfile_id(1);
+        Set<Email> emails = new HashSet<>();
+        Email email = new Email("johnydoe99@gmail.com");
+        emails.add(email);
+        Profile profile = new Profile();
+        profile.setFirstname("John");
+        profile.setLastname("Doe");
+        profile.setEmails(emails);
+        profile.setDob("2010-01-01");
+        profile.setPassword("Password1");
+        profile.setGender("male");
+        profile = profileRepository.save(profile);
+
+        Activity activity = new Activity();
+        activity.setProfile(profile);
+        activity.setActivityName("Run at Hagley Park");
+        activity.setContinuous(true);
+        activity = activityRepository.save(activity);
+
+        SubscriptionHistory subscriptionHistory = new SubscriptionHistory();
+        subscriptionHistory.setActivity(activity);
+        subscriptionHistory.setProfile(profile);
+        subscriptionHistory.setSubscribe(true);
+        subscriptionHistory.setTimeDate(new Date());
+        subscriptionHistory = subscriptionHistoryRepository.save(subscriptionHistory);
+
+        Profile profile1 = profileRepository.findByEmailsContains(email);
+        Set<SubscriptionHistory> subscriptionHistories = subscriptionHistoryRepository.findByProfile_id(profile1.getId());
         org.junit.jupiter.api.Assertions.assertEquals(1, subscriptionHistories.size());
     }
 
+    @Test
+    void testSingleSubscriptionFindByActivity() {
+        Set<Email> emails = new HashSet<>();
+        Email email = new Email("johnydoe99@gmail.com");
+        emails.add(email);
+        Profile profile = new Profile();
+        profile.setFirstname("John");
+        profile.setLastname("Doe");
+        profile.setEmails(emails);
+        profile.setDob("2010-01-01");
+        profile.setPassword("Password1");
+        profile.setGender("male");
+        profile = profileRepository.save(profile);
 
+        Activity activity = new Activity();
+        activity.setProfile(profile);
+        activity.setActivityName("Run at Hagley Park");
+        activity.setContinuous(true);
+        activity = activityRepository.save(activity);
+
+        SubscriptionHistory subscriptionHistory = new SubscriptionHistory();
+        subscriptionHistory.setActivity(activity);
+        subscriptionHistory.setProfile(profile);
+        subscriptionHistory.setSubscribe(true);
+        subscriptionHistory.setTimeDate(new Date());
+        subscriptionHistory = subscriptionHistoryRepository.save(subscriptionHistory);
+
+        Profile profile1 = profileRepository.findByEmailsContains(email);
+        Set<SubscriptionHistory> subscriptionHistories = subscriptionHistoryRepository
+                .findByActivity_id(activityRepository.findByProfile_Id(profile1.getId()).get(0).getId());
+        org.junit.jupiter.api.Assertions.assertEquals(1, subscriptionHistories.size());
+    }
 }
