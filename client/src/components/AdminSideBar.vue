d2429605695130c6f73f39b61cd25f2bc665133b
Cheng Yi Kok <cyk19@uclive.ac.nz>
2020-07-05 14:23:09 +1200

    Implemented axios call in AdminSideBar.vue to assign and remove admin role from another user.
    - Done some styling to the admin privilege buttons to alternate between red and green when adding/removing roles
    - Edit checkUserIsAdmin function to check if user has ROLE_USER_ADMIN
    
    #implement #story[286]

--- client/src/components/AdminSideBar.vue
+++ client/src/components/AdminSideBar.vue
@@ -1,12 +1,18 @@
 <template>
     <div>
-        <b-button v-b-toggle.sidebar-1 v-if="loggedInIsAdmin && loggedInId != $route.params.id" @click="getUserRole()" >Admin</b-button>
+        <b-button @click="getOtherUserRole()" v-b-toggle.sidebar-1
+                  v-if="loggedInIsAdmin && loggedInId != $route.params.id">Admin
+        </b-button>
         <b-sidebar id="sidebar-1" :title="'Admin Controls: ' + userData.firstname + ' ' + userData.lastname" shadow>
             <div class="px-3 py-2">
-                Currently the user is a:   <p v-if="profileIsAdmin">Admin</p> <p v-else>Standard User</p>
+                Currently the user is a: <p v-if="profileIsAdmin">Admin</p>
+                <p v-else>Standard User</p>
                 Possible actions:
-                <b-button v-if="profileIsAdmin" >Remove admin privileges</b-button>
-                <b-button v-else>Add admin privileges</b-button>
+                <br>
+                <b-button @click="toggleAdminRights" v-if="profileIsAdmin" variant="danger">Remove Admin Privilege
+                </b-button>
+                <b-button @click="toggleAdminRights" v-else variant="success">Add Admin Privilege</b-button>
+                <p style="color:red;" v-if="roleEditedErrorMsg != null">{{roleEditedErrorMsg}}</p>
             </div>
         </b-sidebar>
     </div>
@@ -15,6 +21,7 @@
 
 <script>
     import axios from 'axios'
+
     const AdminSideBar = {
         name: "AdminSideBar",
         props: {
@@ -33,17 +40,18 @@
         },
         data: function () {
             return {
-                profileIsAdmin:null
+                profileIsAdmin: null,
+                roleEditedErrorMsg: null
             }
         },
         methods: {
-            getUserRole: function () {
+            getOtherUserRole: function () {
                 const currentObj = this;
                 return axios.get('http://localhost:9499/admin/role/' + this.$route.params.id)
                     .then(function (response) {
-                        currentObj.profileIsAdmin= false
-                        response.data.forEach(function(item) {
-                            if (item.roleName === "ROLE_ADMIN") {
+                        currentObj.profileIsAdmin = false
+                        response.data.forEach(function (item) {
+                            if (item.roleName === "ROLE_ADMIN" || item.roleName === "ROLE_USER_ADMIN") {
                                 currentObj.profileIsAdmin = true;
                             }
                         })
@@ -52,8 +60,19 @@
                         console.log(error)
                     });
             },
-
-
+            toggleAdminRights: function () {
+                let vue = this;
+                axios.put('http://localhost:9499/admin/profiles/' + this.$route.params.id + '/role', {
+                    role: "ROLE_USER_ADMIN"
+                })
+                    .then(function () {
+                        vue.roleEditedErrorMsg = null;
+                        vue.getOtherUserRole();
+                    })
+                    .catch(function (err) {
+                        vue.roleEditedErrorMsg = err.data;
+                    })
+            }
         }
     }
     export default AdminSideBar
