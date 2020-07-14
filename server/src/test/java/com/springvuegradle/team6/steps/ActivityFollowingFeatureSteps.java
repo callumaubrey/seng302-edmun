14ef0a7025ef2b4df843cdb70449293989805940
Harry Collard <htc36@uclive.ac.nz>
2020-07-14 12:47:26 +1200

    Added cucumber tests for following activitys, added to ActivityFollowing.feature and ActivityFollowingFeatureSteps which regarded following activities. Fixed a transient object error with the existing tests by creating local variables. #story[291] #test #peer[lda62, htc36]

--- server/src/test/java/com/springvuegradle/team6/steps/ActivityFollowingFeatureSteps.java
+++ server/src/test/java/com/springvuegradle/team6/steps/ActivityFollowingFeatureSteps.java
@@ -1,10 +1,9 @@
 package com.springvuegradle.team6.steps;
 
-import com.springvuegradle.team6.models.ActivityRepository;
-import com.springvuegradle.team6.models.Email;
-import com.springvuegradle.team6.models.Profile;
-import com.springvuegradle.team6.models.ProfileRepository;
+import com.springvuegradle.team6.models.*;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +15,8 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -37,6 +38,76 @@ public class ActivityFollowingFeatureSteps {
 
   private MockHttpSession session;
 
+  private String jsonString;
+
+  private String profileId;
+
+  private String activityId;
+
+
+  @Given("I create a user with email {string} and password {string}")
+  public void i_registered_a_user_with_email_and_password(String email, String password) throws Exception {
+    jsonString =
+            "{\r\n  \"lastname\": \"Test\",\r\n  \"firstname\": \"Cucumber\",\r\n  \"middlename\": \"Z\",\r\n  \"nickname\": \"BigTest\",\r\n  \"primary_email\": \"" + email + "\",\r\n  \"password\": \"" + password + "\",\r\n  \"bio\": \"Test is plain\",\r\n  \"date_of_birth\": \"2000-11-11\",\r\n  \"gender\": \"nonbinary\"\r\n}";
+    String createProfileUrl = "/profiles";
+    profileId =
+            mvc.perform(
+                    MockMvcRequestBuilders.post(createProfileUrl)
+                            .content(jsonString)
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isCreated())
+                    .andReturn()
+                    .getResponse()
+                    .getContentAsString();
+  }
+
+
+  @Given("I log in with email {string} and password {string}")
+  public void i_log_in_as_a_user_with_email_and_password(String email, String password) throws Exception {
+    session = new MockHttpSession();
+    jsonString =
+            "{\n"
+                    + "\t\"email\": \""
+                    + email
+                    + "\",\n"
+                    + "\t\"password\": \""
+                    + password
+                    + "\"\n"
+                    + "}";
+    String loginUrl = "/login";
+    mvcResponse = mvc.perform(
+            MockMvcRequestBuilders.post(loginUrl)
+                    .content(jsonString)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .session(session));
+  }
+
+  @Given("I create an activity {string}")
+  public void i_create_an_activity(String activityName) throws Exception {
+    String jsonString;
+    jsonString = "{\n" +
+            "  \"activity_name\": \"" + activityName + "\",\n" +
+            "  \"description\": \"tramping iz fun\",\n" +
+            "  \"activity_type\":[ \n" +
+            "    \"Hike\",\n" +
+            "    \"Bike\"\n" +
+            "  ],\n" +
+            "  \"continuous\": true\n" +
+            "}";
+    String createActivityUrl = "/profiles/" + profileId + "/activities";
+    activityId =
+            mvc.perform(
+                    MockMvcRequestBuilders.post(createActivityUrl)
+                            .content(jsonString)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .session(session))
+                    .andExpect(status().isCreated())
+                    .andReturn()
+                    .getResponse()
+                    .getContentAsString();
+
+  }
+
   @Then("User with email {string} home feed shows")
   public void user_with_email_home_feed_shows(
       String emailStr, io.cucumber.datatable.DataTable dataTable) throws Exception {
@@ -45,10 +116,8 @@ public class ActivityFollowingFeatureSteps {
       String activityName = activityMapping.get("Activity Name");
       activityNames.add(activityName);
     }
-    Email email = new Email(emailStr);
-    Profile profile = profileRepository.findByEmailsContains(email);
 
-    String url = "/feed/homefeed/" + profile.getId();
+    String url = "/feed/homefeed/" + profileId;
     mvcResponse =
         mvc.perform(
             MockMvcRequestBuilders.get(url)
@@ -68,5 +137,70 @@ public class ActivityFollowingFeatureSteps {
       responseActivityNames.add(name);
     }
     org.junit.jupiter.api.Assertions.assertTrue(responseActivityNames.containsAll(activityNames));
+  }
+
+  @When("User follows the activity {string}")
+  public void user_follows_the_activity(String activityName) throws Exception{
+    Activity activity = activityRepository.findByActivityName(activityName);
+    String url = "/profiles/"+ profileId + "/subscriptions/activities/" + activity.getId();
+    mvcResponse =
+            mvc.perform(
+                    MockMvcRequestBuilders.post(url)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .session(session));
+  }
+  @Then("User is a follower of {string}")
+  public void user_is_a_follower_of(String activityName) throws Exception{
+    Activity activity = activityRepository.findByActivityName(activityName);
+    String url = "/profiles/"+ profileId + "/subscriptions/activities/" + activity.getId();
+    mvcResponse =
+            mvc.perform(
+                    MockMvcRequestBuilders.get(url)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .session(session));
+    String response =
+            mvc.perform(MockMvcRequestBuilders
+                    .get("/profiles/"+ profileId + "/subscriptions/activities/" + activity.getId())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .session(session)
+            )
+                    .andReturn()
+                    .getResponse()
+                    .getContentAsString();
+    JSONObject obj = new JSONObject(response);
+    org.junit.jupiter.api.Assertions.assertEquals(true, obj.get("subscribed"));
+  }
+
+  @When("User unfollows the activity {string}")
+  public void userUnfollowsTheActivity(String activityName) throws Exception {
+    Activity activity = activityRepository.findByActivityName(activityName);
+    String url = "/profiles/"+ profileId + "/subscriptions/activities/" + activity.getId();
+    mvcResponse =
+            mvc.perform(
+                    MockMvcRequestBuilders.delete(url)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .session(session));
+  }
+
+  @Then("User is not a follower of {string}")
+  public void userIsNotAFollowerOf(String activityName) throws Exception {
+    Activity activity = activityRepository.findByActivityName(activityName);
+    String url = "/profiles/"+ profileId + "/subscriptions/activities/" + activity.getId();
+    mvcResponse =
+            mvc.perform(
+                    MockMvcRequestBuilders.get(url)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .session(session));
+    String response =
+            mvc.perform(MockMvcRequestBuilders
+                    .get("/profiles/"+ profileId + "/subscriptions/activities/" + activity.getId())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .session(session)
+            )
+                    .andReturn()
+                    .getResponse()
+                    .getContentAsString();
+    JSONObject obj = new JSONObject(response);
+    org.junit.jupiter.api.Assertions.assertEquals(false, obj.get("subscribed"));
   }
 }
