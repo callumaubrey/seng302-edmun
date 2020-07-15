8e5d0f68c1c34d31019d7c9eae8987e2c1a5566f
Yu Duan <ydu34@uclive.ac.nz>
2020-07-15 20:16:27 +1200

    Reformat code

--- server/src/test/java/com/springvuegradle/team6/steps/ActivityHashtagFeatureSteps.java
+++ server/src/test/java/com/springvuegradle/team6/steps/ActivityHashtagFeatureSteps.java
@@ -26,173 +26,183 @@ import java.util.Map;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
-@TestPropertySource(properties = {"ADMIN_EMAIL=test@test.com",
-        "ADMIN_PASSWORD=test"})
+@TestPropertySource(properties = {"ADMIN_EMAIL=test@test.com", "ADMIN_PASSWORD=test"})
 @DirtiesContext
 public class ActivityHashtagFeatureSteps {
-    private String jsonString;
-    private MockHttpSession session;
-    private ResultActions mvcResponse;
-    private String profileId;
-    private String activityId;
-    private List<String> autocompleteResult;
-    private List<String> activityNamesByHashtag;
+  private String jsonString;
+  private MockHttpSession session;
+  private ResultActions mvcResponse;
+  private String profileId;
+  private String activityId;
+  private List<String> autocompleteResult;
+  private List<String> activityNamesByHashtag;
 
+  @Autowired private ActivityRepository activityRepository;
 
-    @Autowired
-    private ActivityRepository activityRepository;
+  @Autowired private MockMvc mvc;
 
-    @Autowired
-    private MockMvc mvc;
+  @Given("I registered a user with email {string} and password {string}")
+  public void i_registered_a_user_with_email_and_password(String email, String password)
+      throws Exception {
+    jsonString =
+        "{\r\n  \"lastname\": \"Test\",\r\n  \"firstname\": \"Cucumber\",\r\n  \"middlename\": \"Z\",\r\n  \"nickname\": \"BigTest\",\r\n  \"primary_email\": \""
+            + email
+            + "\",\r\n  \"password\": \""
+            + password
+            + "\",\r\n  \"bio\": \"Test is plain\",\r\n  \"date_of_birth\": \"2000-11-11\",\r\n  \"gender\": \"nonbinary\"\r\n}";
+    String createProfileUrl = "/profiles";
+    profileId =
+        mvc.perform(
+                MockMvcRequestBuilders.post(createProfileUrl)
+                    .content(jsonString)
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isCreated())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+  }
 
-    @Given("I registered a user with email {string} and password {string}")
-    public void i_registered_a_user_with_email_and_password(String email, String password) throws Exception {
-        jsonString =
-                "{\r\n  \"lastname\": \"Test\",\r\n  \"firstname\": \"Cucumber\",\r\n  \"middlename\": \"Z\",\r\n  \"nickname\": \"BigTest\",\r\n  \"primary_email\": \"" + email + "\",\r\n  \"password\": \"" + password + "\",\r\n  \"bio\": \"Test is plain\",\r\n  \"date_of_birth\": \"2000-11-11\",\r\n  \"gender\": \"nonbinary\"\r\n}";
-        String createProfileUrl = "/profiles";
-        profileId =
-                mvc.perform(
-                        MockMvcRequestBuilders.post(createProfileUrl)
-                                .content(jsonString)
-                                .contentType(MediaType.APPLICATION_JSON))
-                        .andExpect(status().isCreated())
-                        .andReturn()
-                        .getResponse()
-                        .getContentAsString();
+  @Given("I log in as a user with email {string} and password {string}")
+  public void i_log_in_as_a_user_with_email_and_password(String email, String password)
+      throws Exception {
+    session = new MockHttpSession();
+    jsonString =
+        "{\n"
+            + "\t\"email\": \""
+            + email
+            + "\",\n"
+            + "\t\"password\": \""
+            + password
+            + "\"\n"
+            + "}";
+    String loginUrl = "/login";
+    mvcResponse =
+        mvc.perform(
+            MockMvcRequestBuilders.post(loginUrl)
+                .content(jsonString)
+                .contentType(MediaType.APPLICATION_JSON)
+                .session(session));
+  }
+
+  @Given("there are no activities in the database")
+  public void there_are_no_activities_in_the_database() {
+    List<Activity> activities = activityRepository.findAll();
+    Assert.assertTrue(activities.size() == 0);
+  }
+
+  @When("I create an activity {string} with hashtags")
+  public void i_create_an_activity_with_hashtags(
+      String activityName, io.cucumber.datatable.DataTable dataTable) throws Exception {
+    List<String> hashTags = new ArrayList<>();
+    for (Map<String, String> hashTagMapping : dataTable.asMaps()) {
+      String hashTag = '"' + hashTagMapping.get("Hashtag") + '"';
+      hashTags.add(hashTag);
     }
 
-    @Given("I log in as a user with email {string} and password {string}")
-    public void i_log_in_as_a_user_with_email_and_password(String email, String password) throws Exception {
-        session = new MockHttpSession();
-        jsonString =
-                "{\n"
-                        + "\t\"email\": \""
-                        + email
-                        + "\",\n"
-                        + "\t\"password\": \""
-                        + password
-                        + "\"\n"
-                        + "}";
-        String loginUrl = "/login";
-        mvcResponse = mvc.perform(
-                MockMvcRequestBuilders.post(loginUrl)
-                        .content(jsonString)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .session(session));
+    jsonString =
+        "{\n"
+            + "  \"activity_name\": \""
+            + activityName
+            + "\",\n"
+            + "  \"description\": \"tramping iz fun\",\n"
+            + "  \"activity_type\":[ \n"
+            + "    \"Hike\",\n"
+            + "    \"Bike\"\n"
+            + "  ],\n"
+            + "  \"continuous\": true,\n"
+            + "  \"hashtags\": "
+            + hashTags
+            + "\n"
+            + "}";
+
+    System.out.println(hashTags);
+    String createActivityUrl = "/profiles/" + profileId + "/activities";
+    mvcResponse =
+        mvc.perform(
+            MockMvcRequestBuilders.post(createActivityUrl)
+                .content(jsonString)
+                .contentType(MediaType.APPLICATION_JSON)
+                .session(session));
+  }
+
+  @Then("I have an activity {string} with hashtags")
+  public void i_have_an_activity_with_hashtags(
+      String activityName, io.cucumber.datatable.DataTable dataTable) throws Exception {
+    Assert.assertTrue(activityRepository.findByActivityName(activityName) != null);
+
+    activityId = mvcResponse.andReturn().getResponse().getContentAsString();
+    String getActivityUrl = "/activities/" + activityId;
+    String responseString =
+        mvc.perform(
+                MockMvcRequestBuilders.get(getActivityUrl)
+                    .content(jsonString)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .session(session))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+
+    for (Map<String, String> hashTagMapping : dataTable.asMaps()) {
+      String hashTag = hashTagMapping.get("Hashtag").toLowerCase().substring(1);
+      Assert.assertTrue(responseString.contains(hashTag));
     }
+  }
 
-    @Given("there are no activities in the database")
-    public void there_are_no_activities_in_the_database() {
-        List<Activity> activities = activityRepository.findAll();
-        Assert.assertTrue(activities.size() == 0);
+  @Then("I do not have an activity {string} and receive {string} status code")
+  public void i_do_not_have_an_activity_and_receive_status_code(
+      String activityName, String statusCode) throws Exception {
+    Assert.assertTrue(activityRepository.findByActivityName(activityName) == null);
+    mvcResponse.andExpect(status().is(Integer.parseInt(statusCode)));
+  }
+
+  @When("I search for hashtag {string}")
+  public void i_search_for_hashtag(String string) throws Exception {
+    String response =
+        mvc.perform(
+                MockMvcRequestBuilders.get("/hashtag/autocomplete?hashtag=number", profileId)
+                    .session(session))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+
+    JSONObject obj = new JSONObject(response);
+    JSONArray arr = obj.getJSONArray("results");
+    List<String> list = new ArrayList<String>();
+    for (int i = 0; i < arr.length(); i++) {
+      list.add(arr.getString(i));
     }
+    autocompleteResult = list;
+  }
 
-    @When("I create an activity {string} with hashtags")
-    public void i_create_an_activity_with_hashtags(String activityName, io.cucumber.datatable.DataTable dataTable) throws Exception {
-        List<String> hashTags = new ArrayList<>();
-        for (Map<String, String> hashTagMapping : dataTable.asMaps()) {
-            String hashTag = '"' + hashTagMapping.get("Hashtag") + '"';
-            hashTags.add(hashTag);
-        }
+  @Then("I get hashtag search results containing")
+  public void i_get_hashtag_search_results_containing(io.cucumber.datatable.DataTable dataTable) {
+    Assert.assertTrue(autocompleteResult.containsAll(dataTable.asList()));
+  }
 
-        jsonString = "{\n" +
-                "  \"activity_name\": \"" + activityName + "\",\n" +
-                "  \"description\": \"tramping iz fun\",\n" +
-                "  \"activity_type\":[ \n" +
-                "    \"Hike\",\n" +
-                "    \"Bike\"\n" +
-                "  ],\n" +
-                "  \"continuous\": true,\n" +
-                "  \"hashtags\": " + hashTags + "\n" +
-                "}";
+  @Then("I get hashtag search results in order")
+  public void i_get_hashtag_search_results_in_order(io.cucumber.datatable.DataTable dataTable) {
+    Assert.assertTrue(autocompleteResult.equals(dataTable.asList()));
+  }
 
-        System.out.println(hashTags);
-        String createActivityUrl = "/profiles/" + profileId + "/activities";
-        mvcResponse =
-                mvc.perform(
-                        MockMvcRequestBuilders.post(createActivityUrl)
-                                .content(jsonString)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .session(session));
-    }
+  @When("I search for activity by hashtag {string}")
+  public void i_search_for_activity_by_hashtag(String string) throws Exception {
+    String response =
+        mvc.perform(
+                MockMvcRequestBuilders.get("/hashtag/someHashTag")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .session(session))
+            .andExpect(status().is2xxSuccessful())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+    JSONArray result = new JSONArray(response);
+    System.out.println(result.length());
+  }
 
-    @Then("I have an activity {string} with hashtags")
-    public void i_have_an_activity_with_hashtags(String activityName, io.cucumber.datatable.DataTable dataTable) throws Exception {
-        Assert.assertTrue(activityRepository.findByActivityName(activityName) != null);
-
-        activityId = mvcResponse.andReturn().getResponse().getContentAsString();
-        String getActivityUrl = "/activities/" + activityId;
-        String responseString =
-                mvc.perform(
-                        MockMvcRequestBuilders.get(getActivityUrl)
-                                .content(jsonString)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .session(session))
-                        .andExpect(status().isOk())
-                        .andReturn()
-                        .getResponse()
-                        .getContentAsString();
-
-        for (Map<String, String> hashTagMapping : dataTable.asMaps()) {
-            String hashTag = hashTagMapping.get("Hashtag").toLowerCase().substring(1);
-            Assert.assertTrue(responseString.contains(hashTag));
-        }
-    }
-
-    @Then("I do not have an activity {string} and receive {string} status code")
-    public void i_do_not_have_an_activity_and_receive_status_code(String activityName, String statusCode) throws Exception {
-        Assert.assertTrue(activityRepository.findByActivityName(activityName) == null);
-        mvcResponse.andExpect(status().is(Integer.parseInt(statusCode)));
-    }
-
-    @When("I search for hashtag {string}")
-    public void i_search_for_hashtag(String string) throws Exception {
-        String response =
-                mvc.perform(
-                        MockMvcRequestBuilders.get("/hashtag/autocomplete?hashtag=number", profileId)
-                                .session(session))
-                        .andExpect(status().isOk())
-                        .andReturn()
-                        .getResponse()
-                        .getContentAsString();
-
-        JSONObject obj = new JSONObject(response);
-        JSONArray arr = obj.getJSONArray("results");
-        List<String> list = new ArrayList<String>();
-        for (int i = 0; i < arr.length(); i++) {
-            list.add(arr.getString(i));
-        }
-        autocompleteResult = list;
-    }
-
-    @Then("I get hashtag search results containing")
-    public void i_get_hashtag_search_results_containing(io.cucumber.datatable.DataTable dataTable) {
-        Assert.assertTrue(autocompleteResult.containsAll(dataTable.asList()));
-    }
-
-    @Then("I get hashtag search results in order")
-    public void i_get_hashtag_search_results_in_order(io.cucumber.datatable.DataTable dataTable) {
-        Assert.assertTrue(autocompleteResult.equals(dataTable.asList()));
-    }
-
-    @When("I search for activity by hashtag {string}")
-    public void i_search_for_activity_by_hashtag(String string) throws Exception {
-        String response =
-                mvc.perform(MockMvcRequestBuilders
-                        .get("/hashtag/someHashTag")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .session(session)
-                )
-                        .andExpect(status().is2xxSuccessful())
-                        .andReturn()
-                        .getResponse()
-                        .getContentAsString();
-        JSONArray result = new JSONArray(response);
-        System.out.println(result.length());
-    }
-
-    @Then("I get the activities in order")
-    public void i_get_the_activities_in_order(io.cucumber.datatable.DataTable dataTable) {
-        Assert.assertTrue(activityNamesByHashtag.equals(dataTable.asList()));
-    }
+  @Then("I get the activities in order")
+  public void i_get_the_activities_in_order(io.cucumber.datatable.DataTable dataTable) {
+    Assert.assertTrue(activityNamesByHashtag.equals(dataTable.asList()));
+  }
 }
