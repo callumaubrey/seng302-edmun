b5a9c927b7c94eef6991cc4357edc14e7a64de45
Harry Collard <htc36@uclive.ac.nz>
2020-07-16 15:21:48 +1200

    Fixed error with brackets in EditActivityRequest.java #fix #story[294]

--- server/src/main/java/com/springvuegradle/team6/requests/EditActivityRequest.java
+++ server/src/main/java/com/springvuegradle/team6/requests/EditActivityRequest.java
@@ -71,5 +71,7 @@ public class EditActivityRequest {
       NamedLocation location =
               new NamedLocation(this.location.country, this.location.state, this.location.city);
       activity.setLocation(location);
+    }
 
+  }
 }
