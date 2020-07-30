ca9473926e85b1b26c8c25f533ba590d87e1bc47
Callum Aubrey <csraubrey@gmail.com>
2020-07-30 15:07:46 +1200

    Added Edmun logo to forbidden component and moved logo into assets directory
    #implement #story[340]

--- client/src/components/ForbiddenMessage.vue
+++ client/src/components/ForbiddenMessage.vue
@@ -1,11 +1,11 @@
 <template>
   <b-container>
     <b-row align-h="center">
-
+      <b-img :src="require('@/assets/goat.jpg')" width="400px"></b-img>
     </b-row>
 
-    <b-row align-h="center">
-      <h3>You are not authorized to view this page.</h3>
+    <b-row align-h="center" style="margin-top:20px;">
+      <h1>You are not authorized to view this page.</h1>
     </b-row>
   </b-container>
 </template>
