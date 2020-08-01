0f1a5ab3b9725e6fe343ae0a56f910f231bbfc04
Callum Aubrey <csraubrey@gmail.com>
2020-08-01 13:50:03 +1200

    ActivityQualificationMetrics entity and repo created. Along with Unit enum.
    #implement #story[357]

---
+++ server/src/main/java/com/springvuegradle/team6/models/entities/ActivityQualificationMetrics.java
@@ -0,0 +1,23 @@
+package com.springvuegradle.team6.models.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class ActivityQualificationMetrics {
+
+  @Id
+  @GeneratedValue
+  private int id;
+
+  @ManyToOne
+  @JoinColumn(name = "activity_id", nullable = false)
+  private Activity activity;
+
+  private boolean rankByAsc;
+
+  private Unit unit;
+}
