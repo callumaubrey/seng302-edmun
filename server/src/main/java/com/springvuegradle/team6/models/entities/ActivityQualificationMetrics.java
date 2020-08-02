ce7441a359890837685919607aac4c14272dc1b4
Callum Aubrey <csraubrey@gmail.com>
2020-08-02 15:42:27 +1200

    Documentation added and also a few more columns added to ActivityQualificationMetrics
    #implement #document #story[357]

--- server/src/main/java/com/springvuegradle/team6/models/entities/ActivityQualificationMetrics.java
+++ server/src/main/java/com/springvuegradle/team6/models/entities/ActivityQualificationMetrics.java
@@ -9,15 +9,37 @@ import javax.persistence.ManyToOne;
 @Entity
 public class ActivityQualificationMetrics {
 
+  /**
+   * Each activity qualification metric instance has its own unique id
+   */
   @Id
   @GeneratedValue
   private int id;
 
+  /**
+   * The title of the qualification metric
+   */
+  private String title;
+
+  /**
+   * The description of the qualification metric
+   */
+  private String description;
+
+  /**
+   * The activity this instance is associated with
+   */
   @ManyToOne
   @JoinColumn(name = "activity_id", nullable = false)
   private Activity activity;
 
+  /**
+   * Boolean to rank metrics in different orders.
+   */
   private boolean rankByAsc;
 
+  /**
+   * The Unit Enmun associated with this instance
+   */
   private Unit unit;
 }
