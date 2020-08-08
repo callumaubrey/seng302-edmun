762ae6e11e30e5296c844a4224615f2609dbfbea
Yu Duan <ydu34@uclive.ac.nz>
2020-08-08 16:44:05 +1200

    Found some broken cucumber tests that required fixing
    - Did some refactoring on the ActivitySharing.feature to have better written feature steps.
    
    #test #story[340]

--- server/src/test/java/com/springvuegradle/team6/steps/LoginSteps.java
+++ server/src/test/java/com/springvuegradle/team6/steps/LoginSteps.java
@@ -1,6 +1,8 @@
 package com.springvuegradle.team6.steps;
 
 import io.cucumber.java.en.Given;
+import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
@@ -19,82 +21,116 @@ public class LoginSteps {
   @Autowired private MockMvc mvc;
 
   @Given("I create a user with email {string} and password {string}")
-  public void i_create_a_user_with_email_and_password(String email, String password) throws Exception {
+  public void i_create_a_user_with_email_and_password(String email, String password)
+      throws Exception {
     jsonString =
-            "{\r\n  \"lastname\": \"Test\",\r\n  \"firstname\": \"Cucumber\",\r\n  \"middlename\": \"Z\",\r\n  \"nickname\": \"BigTest\",\r\n  \"primary_email\": \"" + email + "\",\r\n  \"password\": \"" + password + "\",\r\n  \"bio\": \"Test is plain\",\r\n  \"date_of_birth\": \"2000-11-11\",\r\n  \"gender\": \"nonbinary\"\r\n}";
+        "{\r\n  \"lastname\": \"Test\",\r\n  \"firstname\": \"Cucumber\",\r\n  \"middlename\": \"Z\",\r\n  \"nickname\": \"BigTest\",\r\n  \"primary_email\": \""
+            + email
+            + "\",\r\n  \"password\": \""
+            + password
+            + "\",\r\n  \"bio\": \"Test is plain\",\r\n  \"date_of_birth\": \"2000-11-11\",\r\n  \"gender\": \"nonbinary\"\r\n}";
     String createProfileUrl = "/profiles";
     profileId =
-            mvc.perform(
-                    MockMvcRequestBuilders.post(createProfileUrl)
-                            .content(jsonString)
-                            .contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(status().isCreated())
-                    .andReturn()
-                    .getResponse()
-                    .getContentAsString();
+        mvc.perform(
+                MockMvcRequestBuilders.post(createProfileUrl)
+                    .content(jsonString)
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isCreated())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
   }
-
 
   @Given("I log in with email {string} and password {string}")
   public void i_log_in_with_email_and_password(String email, String password) throws Exception {
     session = new MockHttpSession();
     jsonString =
-            "{\n"
-                    + "\t\"email\": \""
-                    + email
-                    + "\",\n"
-                    + "\t\"password\": \""
-                    + password
-                    + "\"\n"
-                    + "}";
+        "{\n"
+            + "\t\"email\": \""
+            + email
+            + "\",\n"
+            + "\t\"password\": \""
+            + password
+            + "\"\n"
+            + "}";
     String loginUrl = "/login";
-    mvcResponse = mvc.perform(
-            MockMvcRequestBuilders.post(loginUrl)
+    profileId =
+        mvc.perform(
+                MockMvcRequestBuilders.post(loginUrl)
                     .content(jsonString)
                     .contentType(MediaType.APPLICATION_JSON)
-                    .session(session));
+                    .session(session))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+    ;
   }
 
   @Given("I registered a user with email {string} and password {string}")
   public void i_registered_a_user_with_email_and_password(String email, String password)
-          throws Exception {
+      throws Exception {
     jsonString =
-            "{\r\n  \"lastname\": \"Test\",\r\n  \"firstname\": \"Cucumber\",\r\n  \"middlename\": \"Z\",\r\n  \"nickname\": \"BigTest\",\r\n  \"primary_email\": \""
-                    + email
-                    + "\",\r\n  \"password\": \""
-                    + password
-                    + "\",\r\n  \"bio\": \"Test is plain\",\r\n  \"date_of_birth\": \"2000-11-11\",\r\n  \"gender\": \"nonbinary\"\r\n}";
+        "{\r\n  \"lastname\": \"Test\",\r\n  \"firstname\": \"Cucumber\",\r\n  \"middlename\": \"Z\",\r\n  \"nickname\": \"BigTest\",\r\n  \"primary_email\": \""
+            + email
+            + "\",\r\n  \"password\": \""
+            + password
+            + "\",\r\n  \"bio\": \"Test is plain\",\r\n  \"date_of_birth\": \"2000-11-11\",\r\n  \"gender\": \"nonbinary\"\r\n}";
     String createProfileUrl = "/profiles";
     profileId =
-            mvc.perform(
-                    MockMvcRequestBuilders.post(createProfileUrl)
-                            .content(jsonString)
-                            .contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(status().isCreated())
-                    .andReturn()
-                    .getResponse()
-                    .getContentAsString();
+        mvc.perform(
+                MockMvcRequestBuilders.post(createProfileUrl)
+                    .content(jsonString)
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isCreated())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
   }
 
   @Given("I log in as a user with email {string} and password {string}")
   public void i_log_in_as_a_user_with_email_and_password(String email, String password)
-          throws Exception {
+      throws Exception {
     session = new MockHttpSession();
     jsonString =
-            "{\n"
-                    + "\t\"email\": \""
-                    + email
-                    + "\",\n"
-                    + "\t\"password\": \""
-                    + password
-                    + "\"\n"
-                    + "}";
+        "{\n"
+            + "\t\"email\": \""
+            + email
+            + "\",\n"
+            + "\t\"password\": \""
+            + password
+            + "\"\n"
+            + "}";
     String loginUrl = "/login";
-    mvcResponse =
-            mvc.perform(
-                    MockMvcRequestBuilders.post(loginUrl)
-                            .content(jsonString)
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .session(session));
+    profileId =
+        mvc.perform(
+                MockMvcRequestBuilders.post(loginUrl)
+                    .content(jsonString)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .session(session))
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+    ;
+  }
+
+  @Given("the following users are registered")
+  public void the_following_users_are_registered(io.cucumber.datatable.DataTable dataTable)
+      throws Exception {
+    List<Map<String, String>> users = dataTable.asMaps();
+    for (Map<String, String> user : users) {
+      jsonString =
+          "{\r\n  \"lastname\": \"Test\",\r\n  \"firstname\": \"Cucumber\",\r\n  \"middlename\": \"Z\",\r\n  \"nickname\": \"BigTest\",\r\n  \"primary_email\": \""
+              + user.get("email")
+              + "\",\r\n  \"password\": \""
+              + user.get("password")
+              + "\",\r\n  \"bio\": \"Test is plain\",\r\n  \"date_of_birth\": \"2000-11-11\",\r\n  \"gender\": \"nonbinary\"\r\n}";
+      String createProfileUrl = "/profiles";
+      mvc.perform(
+              MockMvcRequestBuilders.post(createProfileUrl)
+                  .content(jsonString)
+                  .contentType(MediaType.APPLICATION_JSON))
+          .andExpect(status().isCreated());
+    }
   }
 }
