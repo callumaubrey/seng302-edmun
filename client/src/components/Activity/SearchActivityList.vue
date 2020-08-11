20108f2fe63ddbede74e6bb6ee42d8de2e4d9fdb
Martin Lopez <mlo53@uclive.ac.nz>
2020-08-12 11:20:39 +1200

    Cleaned the searchactivitylist component up and made a dummy page to be able to see the component.
    Also changed the search component and route to be called searchUsers now instead as we now will have two searches. #implement #fix #story[392]

--- client/src/components/Activity/SearchActivityList.vue
+++ client/src/components/Activity/SearchActivityList.vue
@@ -3,43 +3,46 @@
     <b-card v-for="activity in activity_data" :key="activity.id"
             @click="gotoActivity(activity.id)"
             class="activity-card rounded mb-3">
-      <b-card-body class="activity-card-body">
-        <b-card-title>
+      <b-card-body class="activity-card-body" >
+        <b-row>
 
-          <!-- Type Icon -->
-          <b-icon v-if="activity.continuous"
-                  icon="arrow-repeat" class="rounded-circle bg-info p-2" variant="light" font-scale="1.5"
-                  v-b-tooltip.hover title="Continuous"></b-icon>
-          <b-icon v-else
-                  icon="stopwatch-fill" class="rounded-circle bg-info p-2" variant="light" font-scale="1.5"
-                  v-b-tooltip.hover title="Duration"></b-icon>
-
+          <b-col cols="0.2">
+            <!-- Type Icon -->
+            <b-icon v-if="activity.continuous"
+                    icon="arrow-repeat" class="rounded-circle bg-info p-2" variant="light" font-scale="2.5"
+                    v-b-tooltip.hover title="Continuous"></b-icon>
+            <b-icon v-else
+                    icon="stopwatch-fill" class="rounded-circle bg-info p-2" variant="light" font-scale="2.5"
+                    v-b-tooltip.hover title="Duration"></b-icon>
+          </b-col>
           <!-- Title -->
-          {{activity.activityName}}
+          <b-col class="title">{{activity.activityName}}</b-col>
 
-          <!-- Location -->
-          <span v-if="activity.location" class="float-right text-secondary activity-location-text">
-                    {{activity.location.city}}, {{activity.location.state}}, {{activity.location.country}}
-                </span>
+          <b-col>
+            <span v-if="!activity.continuous" class="date float-right">
+              {{getDurationTimeSummary(activity)}}
+            </span>
+          </b-col>
 
-        </b-card-title>
-        <!-- Duration Start and End Date -->
-        <b-card-sub-title v-if="!activity.continuous" class="date">
-          {{getDurationTimeSummary(activity)}}
-        </b-card-sub-title>
+          <b-col>
+            <!-- Location -->
+            <span v-if="activity.location" class="float-right text-secondary activity-location-text">
+                      {{activity.location.city}}, {{activity.location.state}}, {{activity.location.country}}
+                  </span>
+          </b-col>
+          <b-col cols="0.2" class="activity-type-icon-collection float-right">
+            <ActivityTypeIcon v-for="type in activity.activityTypes" :key="type"
+                              :type_name="type" class="float-right"></ActivityTypeIcon>
+          </b-col>
 
+        </b-row>
         <!-- Hashtags -->
         <b-col cols="8" class="hashtags">
                         <span v-for="tag in activity.tags" :key="tag.name" class="pr-1">
                             <router-link :to="{path: '/hashtag/' + tag.name}">#{{tag.name}}</router-link>
                         </span>
         </b-col>
-        <b-col cols="3" class="activity-type-icon-collection float-right">
-          <ActivityTypeIcon v-for="type in activity.activityTypes" :key="type"
-                            :type_name="type" class="float-right"></ActivityTypeIcon>
-        </b-col>
 
-        <!-- Activity Icons -->
       </b-card-body>
     </b-card>
   </div>
@@ -58,13 +61,44 @@
     },
 
     props: {
-      activity_data: []
+      activity_data: {
+        type: Array
+      }
     },
 
     methods: {
       gotoActivity(id) {
         this.$router.push('/profiles/' + this.user_id + '/activities/' + id);
       },
+      getDurationTimeSummary(activity) {
+        // Returns a pretty print summary of different types of durations
+        // StartDate - EndDate
+        // StartDate Time - EndDate Time
+        // StartDate Time - EndTime
+
+        var start_date = new Date(activity.startTime);
+        var end_date = new Date(activity.endTime);
+        var start_has_time = (start_date.getHours() + start_date.getMinutes() + start_date.getSeconds()) > 0;
+        var end_has_time = (end_date.getHours() + end_date.getMinutes() + end_date.getSeconds()) > 0;
+
+
+        // No time added
+        if(!start_has_time && !end_has_time) {
+          return start_date.toLocaleDateString() + ' - ' + end_date.toLocaleDateString();
+        }
+
+        // Same day
+        if(start_date.toLocaleDateString() === end_date.toLocaleDateString()) {
+          return start_date.toLocaleString() + ' - ' + end_date.toLocaleTimeString();
+        }
+
+        // Default:
+        return start_date.toLocaleString() + ' - ' + end_date.toLocaleString();
+      },
+
+      gotoCreateActivity() {
+        this.$router.push('/profiles/' + this.user_id + '/activities/create');
+      }
 
     }
 
@@ -82,6 +116,7 @@
     cursor: pointer;
     padding: 0px;
     margin: 0px;
+    min-height: 80px;
   }
 
   .activity-type-icon-collection {
@@ -97,13 +132,22 @@
     padding-top: 2px;
     padding-left: 3px;
   }
-  .activity-card-body {
+  .card-body {
     cursor: pointer;
-    padding: 5px;
+    padding-left: 15px;
+    padding-right: 15px;
+    padding-top: 4px;
+    padding-bottom: 4px;
     margin: 0px;
   }
   .date {
     padding-top: 6px;
-    padding-left: 3px;
+    padding-left: 60px;
+    font-size: 90%;
+    font-weight: lighter;
   }
+  .title {
+    font-size: 150%;
+  }
+
 </style>
