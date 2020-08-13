6d96a13cc38d5749f8e690cfebb91b90f5cfeba1
Callum Aubrey <csraubrey@gmail.com>
2020-08-13 18:54:39 +1200

    Cucumber stubs filled out....still got a bit to do
    #test #story[363]

--- server/src/test/java/com/springvuegradle/team6/steps/ActivitySharingFeatureSteps.java
+++ server/src/test/java/com/springvuegradle/team6/steps/ActivitySharingFeatureSteps.java
@@ -11,6 +11,9 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import java.beans.Introspector;
 import java.util.List;
+import java.util.Map;
+import org.json.JSONObject;
+import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
@@ -161,83 +164,302 @@ public class ActivitySharingFeatureSteps {
     Profile profile = profileRepository.findByEmails_address(string);
     List<ActivityRole> activityRole = activityRoleRepository.findByActivity_IdAndProfile_Id(activity.getId(), profile.getId());
     ActivityRole activityRoleFound = activityRole.get(0);
-    org.junit.jupiter.api.Assertions.assertEquals(string2, Introspector.decapitalize(activityRoleFound.getActivityRoleType().toString()));
+    Assert.assertEquals(string2, activityRoleFound.getActivityRoleType().toString());
   }
 
   @When("user {string} adds user roles")
-  public void user_adds_user_roles(String string, io.cucumber.datatable.DataTable dataTable) {
+  public void user_adds_user_roles(String string, io.cucumber.datatable.DataTable dataTable) throws Exception {
+    String password = "Password1";
+    MockHttpSession session = new MockHttpSession();
+    jsonString =
+        "{\n"
+            + "\t\"email\": \""
+            + string
+            + "\",\n"
+            + "\t\"password\": \""
+            + password
+            + "\"\n"
+            + "}";
+    String loginUrl = "/login";
+    mvcResponse =
+        mvc.perform(
+            MockMvcRequestBuilders.post(loginUrl)
+                .content(jsonString)
+                .contentType(MediaType.APPLICATION_JSON)
+                .session(session))
+            .andExpect(status().isOk());
 
+    Profile profile = profileRepository.findByEmails_address(string);
+
+    for (Map<String, String> activityMapping : dataTable.asMaps()) {
+      String email = activityMapping.get("UserEmail");
+      String role = activityMapping.get("Role");
+
+      String jsonString =
+          "{\n"
+              + "  \"subscriber\": { \n"
+              + "  \"email\": \"" + email + "\",\n"
+              + "  \"role\": \"" + role +  "\"\n"
+              + "  }\n"
+              + "}";
+
+      mvcResponse = mvc.perform(
+          MockMvcRequestBuilders.put(
+              "/profiles/" + profile.getId() + "/activities/" + activityId + "/subscriber")
+              .content(jsonString)
+              .contentType(MediaType.APPLICATION_JSON)
+              .session(session));
+    }
   }
 
   @Then("user {string} gets users of role {string} for activity {string} should show")
-  public void user_gets_users_of_role_for_activity_should_show(String string, String string2, String string3, io.cucumber.datatable.DataTable dataTable) {
-    // Write code here that turns the phrase above into concrete actions
-    // For automatic transformation, change DataTable to one of
-    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-    // Double, Byte, Short, Long, BigInteger or BigDecimal.
-    //
-    // For other transformations you can register a DataTableType.
-    throw new io.cucumber.java.PendingException();
+  public void user_gets_users_of_role_for_activity_should_show(
+      String email,
+      String role,
+      String activityName,
+      io.cucumber.datatable.DataTable dataTable) throws Exception {
+    String password = "Password1";
+    MockHttpSession session = new MockHttpSession();
+    jsonString =
+        "{\n"
+            + "\t\"email\": \""
+            + email
+            + "\",\n"
+            + "\t\"password\": \""
+            + password
+            + "\"\n"
+            + "}";
+    String loginUrl = "/login";
+    mvcResponse =
+        mvc.perform(
+            MockMvcRequestBuilders.post(loginUrl)
+                .content(jsonString)
+                .contentType(MediaType.APPLICATION_JSON)
+                .session(session))
+            .andExpect(status().isOk());
+
+    Profile profile = profileRepository.findByEmails_address(email);
+
+    for (Map<String, String> activityMapping : dataTable.asMaps()) {
+      String userEmail = activityMapping.get("UserEmail");
+      String jsonString = "{\n" + "\"email\": \"" + userEmail + "\"\n" + "}";
+
+      String response =
+          mvc.perform(
+              MockMvcRequestBuilders.get(
+                  "/profiles/" + profile.getId() + "/activities/" + activityId + "/subscriber")
+                  .content(jsonString)
+                  .contentType(MediaType.APPLICATION_JSON)
+                  .session(session))
+              .andExpect(status().isOk())
+              .andReturn()
+              .getResponse()
+              .getContentAsString();
+
+      //Assert.assertEquals("User has no activity role", response);
+    }
   }
 
   @When("user {string} removes user")
-  public void user_removes_user(String string, io.cucumber.datatable.DataTable dataTable) {
-    // Write code here that turns the phrase above into concrete actions
-    // For automatic transformation, change DataTable to one of
-    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-    // Double, Byte, Short, Long, BigInteger or BigDecimal.
-    //
-    // For other transformations you can register a DataTableType.
-    throw new io.cucumber.java.PendingException();
+  public void user_removes_user(String email, io.cucumber.datatable.DataTable dataTable) throws Exception {
+    String password = "Password1";
+    MockHttpSession session = new MockHttpSession();
+    jsonString =
+        "{\n"
+            + "\t\"email\": \""
+            + email
+            + "\",\n"
+            + "\t\"password\": \""
+            + password
+            + "\"\n"
+            + "}";
+    String loginUrl = "/login";
+    mvcResponse =
+        mvc.perform(
+            MockMvcRequestBuilders.post(loginUrl)
+                .content(jsonString)
+                .contentType(MediaType.APPLICATION_JSON)
+                .session(session))
+            .andExpect(status().isOk());
+
+    Profile profile = profileRepository.findByEmails_address(email);
+
+    String userEmail = "";
+    for (Map<String, String> activityMapping : dataTable.asMaps()) {
+      userEmail = activityMapping.get("UserEmail");
+    }
+
+    String jsonString = "{\n" + "\"email\": \"" + userEmail + "\"\n" + "}";
+
+    mvcResponse = mvc.perform(
+        MockMvcRequestBuilders.delete(
+            "/profiles/" + profile.getId() + "/activities/" + activityId + "/subscriber")
+            .content(jsonString)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .session(session))
+        .andExpect(status().is2xxSuccessful());
   }
 
   @When("user {string} removes user roles")
-  public void user_removes_user_roles(String string, io.cucumber.datatable.DataTable dataTable) {
-    // Write code here that turns the phrase above into concrete actions
-    // For automatic transformation, change DataTable to one of
-    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-    // Double, Byte, Short, Long, BigInteger or BigDecimal.
-    //
-    // For other transformations you can register a DataTableType.
-    throw new io.cucumber.java.PendingException();
+  public void user_removes_user_roles(String email, io.cucumber.datatable.DataTable dataTable) throws Exception {
+    String password = "Password1";
+    MockHttpSession session = new MockHttpSession();
+    jsonString =
+        "{\n"
+            + "\t\"email\": \""
+            + email
+            + "\",\n"
+            + "\t\"password\": \""
+            + password
+            + "\"\n"
+            + "}";
+    String loginUrl = "/login";
+    mvcResponse =
+        mvc.perform(
+            MockMvcRequestBuilders.post(loginUrl)
+                .content(jsonString)
+                .contentType(MediaType.APPLICATION_JSON)
+                .session(session))
+            .andExpect(status().isOk());
+
+    Profile profile = profileRepository.findByEmails_address(email);
+
+    String userEmail = "";
+    for (Map<String, String> activityMapping : dataTable.asMaps()) {
+      userEmail = activityMapping.get("UserEmail");
+    }
+
+    String jsonString = "{\n" + "\"email\": \"" + userEmail + "\"\n" + "}";
+
+    mvcResponse = mvc.perform(
+        MockMvcRequestBuilders.delete(
+            "/profiles/" + profile.getId() + "/activities/" + activityId + "/subscriber")
+            .content(jsonString)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .session(session))
+        .andExpect(status().is2xxSuccessful());
   }
 
   @When("user {string} changes user roles")
-  public void user_changes_user_roles(String string, io.cucumber.datatable.DataTable dataTable) {
-    // Write code here that turns the phrase above into concrete actions
-    // For automatic transformation, change DataTable to one of
-    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-    // Double, Byte, Short, Long, BigInteger or BigDecimal.
-    //
-    // For other transformations you can register a DataTableType.
-    throw new io.cucumber.java.PendingException();
+  public void user_changes_user_roles(String string, io.cucumber.datatable.DataTable dataTable) throws Exception {
+    String email = "";
+    String oldRole = "";
+    String newRole = "";
+    for (Map<String, String> activityMapping : dataTable.asMaps()) {
+      email = activityMapping.get("UserEmail");
+      oldRole = activityMapping.get("Role");
+      newRole = activityMapping.get("NewRole");
+    }
+
+    String jsonString =
+        "{\n"
+            + "  \"subscriber\": { \n"
+            + "  \"email\": \"" + email + "\",\n"
+            + "  \"role\": \"" + newRole +"\"\n"
+            + "  }\n"
+            + "}";
+
+    mvcResponse =
+        mvc.perform(
+            MockMvcRequestBuilders.put(
+                    "/profiles/" + loginSteps.profileId + "/activities/" + activityId + "/subscriber")
+                .content(jsonString)
+                .contentType(MediaType.APPLICATION_JSON)
+                .session(loginSteps.session));
   }
 
   @When("user {string} deletes activity {string}")
-  public void user_deletes_activity(String string, String string2) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+  public void user_deletes_activity(String email, String activityName) throws Exception {
+    String password = "Password1";
+    MockHttpSession session = new MockHttpSession();
+    jsonString =
+        "{\n"
+            + "\t\"email\": \""
+            + email
+            + "\",\n"
+            + "\t\"password\": \""
+            + password
+            + "\"\n"
+            + "}";
+    String loginUrl = "/login";
+    mvcResponse =
+        mvc.perform(
+            MockMvcRequestBuilders.post(loginUrl)
+                .content(jsonString)
+                .contentType(MediaType.APPLICATION_JSON)
+                .session(session))
+            .andExpect(status().isOk());
+
+    Profile profile = profileRepository.findByEmails_address(email);
+    Activity activity = activityRepository.findByActivityName(activityName);
+
+    mvcResponse = mvc.perform(
+        MockMvcRequestBuilders.delete(
+            "/profiles/{profileId}/activities/{activityId}", profile.getId(), activity.getId())
+            .session(session));
   }
 
   @Then("activity {string} exists")
   public void activity_exists(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+    Activity activity = activityRepository.findByActivityName(string);
+    Assert.assertEquals(string, activity.getActivityName());
   }
 
   @When("the user {string} edits activity {string} description to {string}")
-  public void the_user_edits_activity_description_to(String string, String string2, String string3) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+  public void the_user_edits_activity_description_to(
+      String email,
+      String activityName,
+      String description) throws Exception {
+    String password = "Password1";
+    MockHttpSession session = new MockHttpSession();
+    jsonString =
+        "{\n"
+            + "\t\"email\": \""
+            + email
+            + "\",\n"
+            + "\t\"password\": \""
+            + password
+            + "\"\n"
+            + "}";
+    String loginUrl = "/login";
+    mvcResponse =
+        mvc.perform(
+            MockMvcRequestBuilders.post(loginUrl)
+                .content(jsonString)
+                .contentType(MediaType.APPLICATION_JSON)
+                .session(session))
+            .andExpect(status().isOk());
+
+    Profile profile = profileRepository.findByEmails_address(email);
+    Activity activity = activityRepository.findByActivityName(activityName);
+    jsonString =
+        "{\n"
+            + "  \"activity_name\": \""
+            + activityName
+            + "\",\n"
+            + "  \"description\": \"" + description  + "\",\n"
+            + "  \"activity_type\":[ \n"
+            + "    \"Hike\",\n"
+            + "    \"Bike\"\n"
+            + "  ],\n"
+            + "  \"visibility\": \""
+            + activity.getVisibilityType()
+            + "\",\n"
+            + "  \"continuous\": true"
+            + "}";
+    String editActivityUrl = "/profiles/" + profile.getId() + "/activities";
+    mvcResponse = mvc.perform(
+        MockMvcRequestBuilders.put(editActivityUrl)
+            .content(jsonString)
+            .contentType(MediaType.APPLICATION_JSON)
+            .session(session));
   }
 
   @Then("the activity {string} should have description {string}")
   public void the_activity_should_have_description(String string, String string2) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+    Activity activity = activityRepository.findByActivityName(string);
+    Assert.assertEquals(string2, activity.getDescription());
   }
 }
