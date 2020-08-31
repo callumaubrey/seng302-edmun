ca0c43df190da7caa8eb3b5bb3f4e603b769d86e
Louis Davies <lda62@uclive.ac.nz>
2020-08-31 20:14:02 +1200

    After Callums help finished up refactoring the endpoint including adding more extensive responses and documented new code better. #implement #test #story[408]

--- server/src/main/java/com/springvuegradle/team6/requests/LocationUpdateRequest.java
+++ server/src/main/java/com/springvuegradle/team6/requests/LocationUpdateRequest.java
@@ -5,10 +5,11 @@ import javax.validation.constraints.NotNull;
 
 public class LocationUpdateRequest {
 
+  /** double value for exact latitude coordinate */
   @NotNull(message = "latitude cannot be null")
   public double latitude;
 
+  /** double value for exact longitude coordinate */
   @NotNull(message = "longitude cannot be null")
   public double longitude;
-
 }
