bd5a75f7b381933570212102b8da48b446c19f62
Louis Davies <lda62@uclive.ac.nz>
2020-09-07 20:10:57 +1200

    Created activity distance slider component - Created slider component - emits distance when changed - added jest test #implement #test #pair[lda62, ewm27] #story[412]

---
+++ client/src/components/ActivityDistanceSlider.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="slidecontainer">
+        <input type="range" min="1" max="100" v-model="distance" class="slider" id="distance-slider" @change="emitDistance">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ActivityDistanceSlider",
+        data() {
+            return {
+                distance: 50,
+            }
+        },
+        methods: {
+            emitDistance: function() {
+                this.$emit('distanceChange', this.distance);
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .slider {
+        width: 100%; /* Full-width */
+    }
+</style>
--- client/src/pages/Login/App.vue
+++ client/src/pages/Login/App.vue
@@ -52,6 +52,7 @@
         <b-container class="b-container">
             <b-link to="/register" id="signup-link">Sign up for Edmun</b-link>
         </b-container>
+        <ActivityDistanceSlider></ActivityDistanceSlider>
 
     </div>
 
@@ -61,10 +62,12 @@
 import NavBar from '@/components/NavBar.vue';
 import api from '@/Api';
 import {mutations} from "../../store";
+import ActivityDistanceSlider from "../../components/ActivityDistanceSlider";
 
 export default {
   components: {
-    NavBar
+    NavBar,
+      ActivityDistanceSlider
   },
   computed: {
     emailState() {
