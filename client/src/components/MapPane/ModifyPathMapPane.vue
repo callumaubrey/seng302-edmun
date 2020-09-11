80accf98400ef1aca759882c4dd90b1ff95433b9
Callum Aubrey <csraubrey@gmail.com>
2020-09-11 13:23:27 +1200

    Reformatted Harrys code and added reset button for coordinates.
    #implement #story[458]

--- client/src/components/MapPane/ModifyPathMapPane.vue
+++ client/src/components/MapPane/ModifyPathMapPane.vue
@@ -1,68 +1,70 @@
 <template>
-    <div>
-        <b-form-group label="Using sub-components:">
-            <b-form-checkbox-group id="checkbox-group-2" v-model="autoRoute" name="flavour-2" v-if="canChangeSelection">
-                <b-form-checkbox value="false" >Selection</b-form-checkbox>
-                <b-form-checkbox value="true">Auto route</b-form-checkbox>
-            </b-form-checkbox-group>
-        </b-form-group>
-        <map-pane @onMapClick="mapClicked" ref="map" ></map-pane>
-    </div>
+  <div>
+    <b-form-group label="Using sub-components:">
+      <b-form-checkbox-group id="checkbox-group-2" v-model="autoRoute" name="flavour-2" v-if="canChangeSelection">
+        <b-form-checkbox value="false">Selection</b-form-checkbox>
+        <b-form-checkbox value="true">Auto route</b-form-checkbox>
+      </b-form-checkbox-group>
+    </b-form-group>
+    <b-button @click="resetMarkers()">Reset</b-button>
+    <map-pane @onMapClick="mapClicked" ref="map"></map-pane>
+  </div>
 </template>
 
 <script>
-    import MapPane from "./MapPane";
-    import axios from 'axios'
-    export default {
-        name: "RecordActivityResultModal",
-        components: {
-            MapPane
-        },
-        props: ['profileId', 'activityId', 'loggedInId'],
-        data() {
-            return {
-                autoRoute: "false",
-                canChangeSelection: true,
-                count : 0
-            }
-        },
-        methods: {
-            mapClicked(event) {
-                const currObj = this
-                const marker = this.$refs.map.markers[this.$refs.map.markers.length-1]
-                if (this.autoRoute =="true" && marker != null) {
-                    axios.post("https://api.openrouteservice.org/v2/directions/driving-car/geojson" ,
-                        {
-                            coordinates: [[marker.position[1], marker.position[0]],
-                                [event.latlng.lng, event.latlng.lat]]
-                        }, {headers : {Authorization : "5b3ce3597851110001cf6248183abbef295f42049b13e7a011f98247"}})
-                    .then(function (res) {
-                        for (var iii of res.data.features[0].geometry.coordinates) {
-                          currObj.$refs.map.routePoints.push([iii[1], iii[0]])
-                        }
-                        this.$refs.map.createMarker(this.count, 0, event.latlng.lat, event.latlng.lng, "", "")
-                    })
-                }else {
-                    this.$refs.map.routePoints.push([event.latlng.lat, event.latlng.lng])
-                    this.$refs.map.createMarker(this.count, 0, event.latlng.lat, event.latlng.lng, "", "")
-                }
-                if (this.$refs.map.routePoints.length == 0) {
-                    this.canChangeSelection = true
-                }else {
-                    this.canChangeSelection = false
-                }
+import MapPane from "./MapPane";
+import axios from 'axios'
 
-
-            }
-        },
-        mounted() {
-            this.$refs.map.tooltip = false
-        }
+export default {
+  name: "RecordActivityResultModal",
+  components: {
+    MapPane
+  },
+  props: ['profileId', 'activityId', 'loggedInId'],
+  data() {
+    return {
+      autoRoute: "false",
+      canChangeSelection: true,
+      count: 0
     }
+  },
+  methods: {
+    mapClicked(event) {
+      const currObj = this
+      const marker = this.$refs.map.markers[this.$refs.map.markers.length - 1]
+      if (this.autoRoute == "true" && marker != null) {
+        axios.post("https://api.openrouteservice.org/v2/directions/driving-car/geojson",
+            {
+              coordinates: [[marker.position[1], marker.position[0]],
+                [event.latlng.lng, event.latlng.lat]]
+            }, {headers: {Authorization: "5b3ce3597851110001cf6248183abbef295f42049b13e7a011f98247"}})
+            .then(function (res) {
+              for (var coord of res.data.features[0].geometry.coordinates) {
+                currObj.$refs.map.routePoints.push([coord[1], coord[0]])
+              }
+              this.$refs.map.createMarker(this.count, 0, event.latlng.lat, event.latlng.lng, "", "")
+            })
+      } else {
+        this.$refs.map.routePoints.push([event.latlng.lat, event.latlng.lng])
+        this.$refs.map.createMarker(this.count, 0, event.latlng.lat, event.latlng.lng, "", "")
+      }
+      if (this.$refs.map.routePoints.length == 0) {
+        this.canChangeSelection = true
+      } else {
+        this.canChangeSelection = false
+      }
+    },
+    resetMarkers() {
+      this.$refs.map.markers = [];
+      this.$refs.map.routePoints = [];
+    }
+  },
+  mounted() {
+    this.$refs.map.tooltip = false
+  }
+}
 
 </script>
-
-
 
 
 <style scoped>
