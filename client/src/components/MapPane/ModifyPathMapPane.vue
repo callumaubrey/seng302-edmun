3225c10587424ea12ee0dcfc3f26aed7e73cec64
Harry Collard <htc36@uclive.ac.nz>
2020-09-11 14:27:09 +1200

    Added back functionality for auto route option in ModifyPathMapPane.vue #implement #story[458]

--- client/src/components/MapPane/ModifyPathMapPane.vue
+++ client/src/components/MapPane/ModifyPathMapPane.vue
@@ -8,7 +8,7 @@
       </b-form-checkbox-group>
     </b-form-group>
     <b-button @click="resetMarkerAndPoint()">Reset</b-button>
-    <div v-if="this.autoRoute == 'false'">
+    <div>
       <b-button @click="prevPoint()">Back</b-button>
     </div>
     <map-pane @onMapClick="mapClicked" ref="map"></map-pane>
@@ -36,25 +36,8 @@ export default {
     mapClicked(event) {
       const currObj = this
       const marker = this.$refs.map.markers[this.$refs.map.markers.length - 1]
-      let listOfWayPoints = []
       if (this.autoRoute == "true" && marker != null) {
-        for (var waypoint of this.$refs.map.markers) {
-          listOfWayPoints.push([waypoint.position[1], waypoint.position[0]])
-        }
-        listOfWayPoints.push([event.latlng.lng, event.latlng.lat])
-        axios.post("https://api.openrouteservice.org/v2/directions/driving-car/geojson",
-            {
-              coordinates: listOfWayPoints,
-            },
-            {headers: {Authorization: "5b3ce3597851110001cf6248183abbef295f42049b13e7a011f98247"}})
-        .then(function (res) {
-          currObj.$refs.map.createMarker(currObj.count, 0, event.latlng.lat, event.latlng.lng, "",
-              "")
-          currObj.$refs.map.routePoints = []
-          for (var iii of res.data.features[0].geometry.coordinates) {
-            currObj.$refs.map.routePoints.push([iii[1], iii[0]])
-          }
-        })
+        this.getRoutePoints(event)
       } else {
         this.$refs.map.routePoints.push([event.latlng.lat, event.latlng.lng])
         this.$refs.map.createMarker(this.count, 0, event.latlng.lat, event.latlng.lng, "", "")
@@ -68,6 +51,31 @@ export default {
       currObj.count += 1
 
     },
+    getRoutePoints(event){
+      const currObj = this
+      let listOfWayPoints = []
+      for (var waypoint of this.$refs.map.markers) {
+        listOfWayPoints.push([waypoint.position[1], waypoint.position[0]])
+      }
+      if (event != false){
+        listOfWayPoints.push([event.latlng.lng, event.latlng.lat])
+      }
+      axios.post("https://api.openrouteservice.org/v2/directions/driving-car/geojson",
+          {
+            coordinates: listOfWayPoints,
+          },
+          {headers: {Authorization: "5b3ce3597851110001cf6248183abbef295f42049b13e7a011f98247"}})
+      .then(function (res) {
+        if (event != false) {
+          currObj.$refs.map.createMarker(currObj.count, 0, event.latlng.lat, event.latlng.lng, "",
+              "")
+        }
+        currObj.$refs.map.routePoints = []
+        for (var iii of res.data.features[0].geometry.coordinates) {
+          currObj.$refs.map.routePoints.push([iii[1], iii[0]])
+        }
+      })
+    },
     resetMarkerAndPoint() {
       this.$refs.map.markers = [];
       this.$refs.map.routePoints = [];
@@ -77,6 +85,13 @@ export default {
       if (this.$refs.map.markers.length > 1) {
         this.$refs.map.markers.pop();
         this.$refs.map.routePoints.pop();
+        if(this.autoRoute == "true") {
+          if (this.$refs.map.markers.length == 1) {
+            this.$refs.map.routePoints = []
+          }else {
+            this.getRoutePoints(false)
+          }
+        }
       }
     }
   },
