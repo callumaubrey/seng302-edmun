d55caadfcba71002f401bbbbf4b11597ab602930
Martin Lopez <mlo53@uclive.ac.nz>
2020-09-15 13:54:19 +1200

    Token is now generated on creation of PasswordToken.
    Created tests to make sure token is automatically generated. Also tested the relationship works. #test #implement #story[460]

--- server/src/main/java/com/springvuegradle/team6/models/entities/PasswordToken.java
+++ server/src/main/java/com/springvuegradle/team6/models/entities/PasswordToken.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import java.security.SecureRandom;
 
 @Entity
 public class PasswordToken {
@@ -17,6 +18,9 @@ public class PasswordToken {
 
     private String token;
 
+    static final String ABC = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    static SecureRandom rnd = new SecureRandom();
+
     public PasswordToken(){}
 
     public PasswordToken(Profile profile) {
@@ -24,13 +28,28 @@ public class PasswordToken {
         generateToken();
     }
 
+    public void setProfile(Profile profile) { this.profile = profile; }
+
+    /**
+     * Generates a random token of length 16
+     */
     private void generateToken() {
-        // return randomized token
-        String token = " ";
-        this.token = token;
+        this.token = randomString();
     }
 
-    public void setProfile(Profile profile) {
-        this.profile = profile;
+    public String getToken() { return token; }
+
+    public Profile getProfile() { return profile; }
+
+    /**
+     * Creates a random string of length 16
+     * @return The random string of length 16
+     */
+    String randomString(){
+        int len = 16;
+        StringBuilder sb = new StringBuilder(len);
+        for(int i = 0; i < len; i++)
+            sb.append(ABC.charAt( rnd.nextInt(ABC.length())));
+        return sb.toString();
     }
 }
