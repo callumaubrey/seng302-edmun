b23983ea961717de1a651df2721bb4eb4405468c
Martin Lopez <mlo53@uclive.ac.nz>
2020-09-17 13:50:34 +1200

    Configured the ConfigProperties correctly.
    Still struggling to find a way to use this in the Application file when starting up the mail Sender. #fix #story[460]

--- server/src/main/java/com/springvuegradle/team6/security/ConfigProperties.java
+++ server/src/main/java/com/springvuegradle/team6/security/ConfigProperties.java
@@ -2,18 +2,31 @@ package com.springvuegradle.team6.security;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
-@PropertySource("classpath:application.properties")
+@ConfigurationProperties("spring.mail")
 public class ConfigProperties {
 
-    @Autowired
-    private Environment env;
+    private String username;
+    private String password;
 
-    public String getConfigValue(String configKey){
-        return env.getProperty(configKey);
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
