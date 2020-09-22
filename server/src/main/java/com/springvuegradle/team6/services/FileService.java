a5f93a1d9d7ab3da28828d1576bcdb7bbc318ac3
Louis Davies <lda62@uclive.ac.nz>
2020-09-22 13:05:22 +1200

    Created user photo put endpoint #implement #story[463]

--- server/src/main/java/com/springvuegradle/team6/services/FileService.java
+++ server/src/main/java/com/springvuegradle/team6/services/FileService.java
@@ -36,6 +36,7 @@ public class FileService {
     public String uploadProfileImage(MultipartFile file, int profileId) {
         try {
             String fileName = "profile" + profileId + "." + file.getContentType().replaceFirst("image/", "");
+            System.out.println(profileDirectory + fileName);
             File newFile = new File(profileDirectory + fileName);
             newFile.createNewFile();
             BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(newFile));
