b68ad40902422ad463cdee468d7fc699c81ff697
Martin Lopez <mlo53@uclive.ac.nz>
2020-09-22 12:02:41 +1200

    FileService now successfully creates images in the directory specified in app properties.
    #fix #implement #story[464]

--- server/src/main/java/com/springvuegradle/team6/services/FileService.java
+++ server/src/main/java/com/springvuegradle/team6/services/FileService.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -26,32 +29,39 @@ public class FileService {
     /**
      * This function will take a file and save it in the directory given in the applciation properties.
      * It replaces any existing file with the same name.
+     *
      * @param file the file to be saved
      * @return returns null if successful, a response entity if failed.
      */
     public ResponseEntity<String> uploadProfileImage(MultipartFile file, int profileId) {
         try {
-            Path copyLocation = createPath(profileDirectory,"profile",profileId, file.getContentType());
-            Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
-            return null;
-        } catch (Exception e) {
-            return new ResponseEntity<>("Could not save image", HttpStatus.INTERNAL_SERVER_ERROR);
+            String path = profileDirectory + "profile" + profileId + "." + file.getContentType().replaceFirst("image/", "");
+            File newFile = new File(path);
+            newFile.createNewFile();
+            BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(newFile));
+            stream.write(file.getBytes());
+            stream.close();
+        } catch (IOException ex) {
+            return new ResponseEntity<>("Failed to upload image", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+        return null;
     }
 
     public ResponseEntity<String> uploadActivityImage(MultipartFile file, int activityId) {
         try {
-            Path copyLocation = createPath(activityDirectory,"activity",activityId, file.getContentType());
-            Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
-            return null;
-        } catch (Exception e) {
-            return new ResponseEntity<>("Could not save image", HttpStatus.INTERNAL_SERVER_ERROR);
+            String path = activityDirectory + "activity" + activityId + "." + file.getContentType().replaceFirst("image/", "");
+            File newFile = new File(path);
+            newFile.createNewFile();
+            BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(newFile));
+            stream.write(file.getBytes());
+            stream.close();
+        } catch (IOException ex) {
+            return new ResponseEntity<>("Failed to upload image", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+        return null;
     }
 
-    public Path createPath(String directory, String entityType, int id, String fileType) {
-        return Paths.get(directory + File.separator + StringUtils.cleanPath(entityType + id + fileType));
-    }
+
 
 
 
